e77adacd5ed338977dc6f08b49bc62274290c7db
Nelson Gomes Neto <ngn@ic.ufal.br>
2017-09-14 14:39:09 -0300

    Weee

--- Huxley/contandodigitospares.c
+++ Huxley/contandodigitospares.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+int countPairDigits(long long int num)
+{
+  if (num < 10)
+    return(!(num % 10 & 1));
+  return(!(num % 10 & 1) + countPairDigits(num / 10));
+}
+
 int main()
 {
-  char num[99]; scanf("%s", num);
-  int check, pares = 0;
-  for (check = 0; check < strlen(num); check ++)
-  {
-    //printf("%d\n", (int) num[check] - 48);
-    if ((int) (num[check] - 48) % 2 == 0)
-    {
-      pares ++;
-    }
-  }
-  printf("%d\n", pares);
+  long long int num; scanf("%Ld", &num);
+  int digits = countPairDigits(num);
+  printf("%d\n", digits);
   return(0);
 }
