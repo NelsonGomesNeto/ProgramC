687bde64683489da9abb2b4f0fa93a78e5f7d23a
NelsonGomesNeto <ngn@ic.ufal.br>
2017-09-18 10:48:19 -0300

    LoL

--- Huxley/603Orkut.py
+++ Huxley/603Orkut.py
@@ -1,21 +1,72 @@
-friendsSize = int(input())
-friends = list(input().split())
-stringToInt = dict(zip(friends, range(0, friendsSize)))
-intToString = dict(zip(range(0, friendsSize), friends))
-print(stringToInt, intToString)
+from queue import *
+DEBUG = 0
 
-graph = [[] for i in range(friendsSize)]
-for i in range(friendsSize):
-    line = input().split()
-    u, uFriends = line[0], line[1:]
-    for v in uFriends[1:]:
-        graph[stringToInt[u]] += [stringToInt[v]]
+def topologicalSortBFS(graph):
+    inDegree = [0] * len(graph)
+    for u in graph:
+        for v in u:
+            inDegree[v] += 1
+    queue = Queue()
+    for i, quantity in enumerate(inDegree):
+        if (quantity == 0):
+            queue.put(i)
 
-print(graph)
-for i, u in enumerate(graph):
-    print(intToString[i], end='')
-    for v in u:
-        print('->', intToString[v], end = ' ')
-    print()
+    answer, counter = [], 0
+    while(not queue.empty()):
+        curr = queue.get()
+        #print("now", curr)
+        answer += [curr]
 
-# Do BFS Topological Sort!!!!
+        for u in graph[curr]:
+            inDegree[u] -= 1
+            if (inDegree[u] == 0):
+                queue.put(u)
+
+        counter += 1
+
+    if (counter != len(graph)):
+        return([])
+    else:
+        return(answer)
+
+run = 0
+while (True):
+    friendsSize = int(input())
+    if (friendsSize == 0):
+        break
+    friends = list(input().split())
+    stringToInt = dict(zip(friends, range(0, friendsSize)))
+    intToString = dict(zip(range(0, friendsSize), friends))
+    if (DEBUG):
+        print(stringToInt, intToString)
+
+    graph = [[] for i in range(friendsSize)]
+    for i in range(friendsSize):
+        line = input().split()
+        u, uFriends = line[0], line[1:]
+        for v in uFriends[1:]:
+            graph[stringToInt[u]] += [stringToInt[v]]
+
+    if (DEBUG):
+        print(graph)
+        for i, u in enumerate(graph):
+            print(intToString[i], end='')
+            for v in u:
+                print('->', intToString[v], end = ' ')
+            print()
+
+    if (run > 0):
+        print()
+    run += 1
+    print("Teste", run)
+    answer = topologicalSortBFS(graph)
+    if (len(answer) == 0):
+        print("impossivel")
+    else:
+        answer.reverse()
+        print(intToString[answer[0]], end='')
+        for i in answer[1:]:
+            print(' ', intToString[i], sep='', end='')
+        print()
+
+    # Do BFS Topological Sort!!!!
