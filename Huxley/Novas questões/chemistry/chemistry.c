1b2f9ccbbdd9f44cb2901f89d23e8d143793094b
Nelson Gomes Neto <ngn@ic.ufal.br>
2017-10-26 00:34:58 -0300

    Fixed, now I need to create test cases

--- Huxley/Novas questões/chemistry/chemistry.c
+++ Huxley/Novas questões/chemistry/chemistry.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 void printMatrix(int y, int x, int mat[][x])
 {
-  int i, j;
+  int i, j, k, biggest = log10(mat[y-1][x-1]);
   for (i = 0; i < y; i ++)
   {
     for (j = 0; j < x; j ++)
+    {
+      for (k = log10(mat[i][j]); k < biggest; k ++)
+        printf(" ");
       printf("%d ", mat[i][j]);
+    }
     printf("\n");
   }
+}
+
+int min(int a, int b)
+{
+  return(a < b ? a : b);
+}
+
+int max(int a, int b)
+{
+  return(a > b ? a : b);
 }
 
 int main()
@@ -16,11 +31,23 @@ int main()
   int y, x, i, j; scanf("%d %d", &y, &x);
   int mat[y][x];
   int go = 1, now = 1;
-  for (i = 0, j = 0; i < y; i ++, j ++)
+  for (i = 0; i < y + x - 1; i ++)
   {
-    if (i > y) i = y - 1;
-    if (j > x) j = x - 1;
-
+    j = 0;
+    while (j < go)
+    {
+      printf("%d %d %d\n", i - j, y - 1 - j, j + (i / y));
+      mat[min(i, y - 1) - j][j + max(i - y + 1, 0)] = now ++;
+      j ++;
+    }
+    // j = 0;
+    // while (j < go)
+    // {
+    //   if (i - j >= 0 && i - j < y  && j >= 0 && j < x)
+    //     mat[i - j][j] = now ++;
+    //   j ++;
+    // }
+    go = y - (y - i) + 2;
   }
 
   printMatrix(y, x, mat);
