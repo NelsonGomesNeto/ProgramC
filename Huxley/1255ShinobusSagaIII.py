d76606d027e3c0f4cfe4433e7224f66931219e33
Nelson Gomes Neto <ngn@ic.ufal.br>
2017-10-28 21:01:06 -0300

    LoL, Mania de Par is really breaking my mind right now...

--- Huxley/1255ShinobusSagaIII.py
+++ Huxley/1255ShinobusSagaIII.py
@@ -2,18 +2,17 @@ from heapq import *
 inf = 2**33
 
 def dijkstra(graph, monster, cost, visited, start, end):
-    cost[start] = 0
+    cost[start][0] = 0
     pq = []
-    heappush(pq, [cost[start], start])
+    heappush(pq, [cost[start][0], start])
     while (pq):
         time, v = heappop(pq)
-        if (visited[v] == monster[v]): continue
-        visited[v] += 1
+        #print(time, v)
         for u in graph[v]:
             if ((time + 1) % monster[u] != 0):
-                if (time + 1 < cost[u]):
-                    cost[u] = time + 1
-                heappush(pq, [time + 1, u])
+                if (time + 1 < cost[u][(time + 1) % (max(monster) + 1)]):
+                    cost[u][(time + 1) % (max(monster) + 1)] = time + 1
+                    heappush(pq, [time + 1, u])
 
 #main
 rooms, start, end, corridors = map(int, input().split())
@@ -23,11 +22,12 @@ for i in range(corridors):
     u, v = map(int, input().split())
     graph[u] += [v]
     #graph[v] += [u]
-cost, visited = [inf] * rooms, [0] * rooms
-dijkstra(graph, monster, cost, visited, start, rooms - 1)
+cost, visited = [[inf] * (max(monster) + 1) for i in range(rooms)], [0] * rooms
+dijkstra(graph, monster, cost, visited, start, end)
 #print(visited)
 #print(cost)
-if (cost[end] == inf):
+#print(cost[end])
+if (min(cost[end]) == inf):
     print("Araragi morreu")
 else:
-    print(cost[end], "minutos")
+    print(min(cost[end]), "minutos")
