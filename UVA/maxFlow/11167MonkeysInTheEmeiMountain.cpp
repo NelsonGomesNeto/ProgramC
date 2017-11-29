0a8463e2c9232ddce4a1bad5c75b73133d9505de
NelsonGomesNeto <ngn@ic.ufal.br>
2017-11-29 17:56:45 -0300

    FINALLY REALIZED (READ A SMALL HINT) HOW TO MAKE THIS FREAKING FAST!!!!

--- UVA/maxFlow/11167MonkeysInTheEmeiMountain.cpp
+++ UVA/maxFlow/11167MonkeysInTheEmeiMountain.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#define DEBUG if(1)
 using namespace std;
-const int maxVertices = 2 + 100 + 50000;
-map<int, int> matrixGraph[maxVertices]; int level[maxVertices], inf = 1<<20;
+const int maxVertices = 2 + 100 + 100000;
+int **matrixGraph; int level[maxVertices], inf = 1<<20;
 
 int bfs(vector<int> graph[], int source, int target)
 {
@@ -30,7 +31,7 @@ int dfs(vector<int> graph[], int u, int target, int flow)
       int pathFlow = dfs(graph, v, target, min(flow, matrixGraph[u][v]));
       flow -= pathFlow;
       matrixGraph[u][v] -= pathFlow;
-      matrixGraph[v][u] -= pathFlow;
+      matrixGraph[v][u] += pathFlow;
       actualFlow += pathFlow;
     }
   return(actualFlow);
@@ -54,49 +55,56 @@ void addEdge(vector<int> graph[], int u, int v, int c)
   graph[u].push_back(v);
   matrixGraph[u][v] = c;
   graph[v].push_back(u);
+  matrixGraph[v][u] = 0;
 }
 
 int main()
 {
-  int monkeys, canDrink;
+  matrixGraph = (int**) malloc(maxVertices * sizeof(int*));
+  for (int i = 0; i < maxVertices; i ++)
+    matrixGraph[i] = (int*) malloc(maxVertices * sizeof(int));
+
+  int monkeys, canDrink, test = 1;
   while (scanf("%d %d", &monkeys, &canDrink) != EOF && !(monkeys == 0))
   {
-    for (int i = 0; i < maxVertices; i ++) matrixGraph[i].clear();
     vector<int> graph[maxVertices];
-
-    for (int i = 100; i < 25100; i ++)
+    for (int i = 100; i < 50100; i ++)
     {
-      addEdge(graph, i, i + 25000, canDrink);
-      //addEdge(graph, i + 1, i + 25000, canDrink);
-      addEdge(graph, i + 25000, 50101, canDrink);
+      addEdge(graph, i, i + 50000, canDrink);
+      addEdge(graph, i + 50000, maxVertices - 1, inf);
     }
 
-    int toDrink = 0;
+    int toDrink = 0, monkey[monkeys][2];
     for (int i = 0; i < monkeys; i ++)
     {
       int v, a, b; scanf("%d %d %d", &v, &a, &b);
+      monkey[i][0] = a; monkey[i][1] = b;
       toDrink += v;
       addEdge(graph, 0, i + 1, v);
-      for (int j = a; j < b; j ++)
-      {
+      for (int j = a; j <= b - 1; j ++)
         addEdge(graph, i + 1, j + 100, 1);
-        addEdge()
+    }
+
+    int maxFlow = dinic(graph, 0, maxVertices - 1);
+
+    printf("Case %d: %s\n", test ++, maxFlow == toDrink ? "Yes" : "No");
+    if (maxFlow == toDrink)
+      for (int i = 0; i < monkeys; i ++)
+      {
+        int drink[maxVertices][2], at = 0;
+        for (int j = monkey[i][0]; j < monkey[i][1]; j ++)
+          //printf("%d %d (%d)\n", i, j, matrixGraph[j + 100][i + 1]);
+          if (matrixGraph[j + 100][i + 1])
+          {
+            drink[at][0] = j;
+            while (j < monkey[i][1] && matrixGraph[j + 100][i + 1])
+              j ++;
+            drink[at ++][1] = j;
+          }
+        printf("%d", at);
+        for (int j = 0; j < at; j ++) printf(" (%d,%d)", drink[j][0], drink[j][1]);
+        printf("\n");
       }
-    }
-
-
-    int maxFlow = dinic(graph, 0, 50101);
-
-    for (int i = 0; i < monkeys; i ++)
-    {
-      printf("%d", i + 1);
-      for (auto j: graph[i + 1])
-        if (matrixGraph[j][i + 1])
-          printf(" -> %d", j);
-      printf("\n");
-    }
-
-    printf("%d maxFlow: %d\n", maxFlow == toDrink, maxFlow);
   }
 
   return(0);
---
+++ UVA/maxFlow/FOCK11167MonkeysInTheEmeiMountain.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+#define DEBUG if(1)
+using namespace std;
+const int maxVertices = 2 + 100 + 1000;
+int **matrixGraph; int level[maxVertices], inf = 1<<20;
+
+int bfs(int **graph, int source, int target)
+{
+  memset(level, -1, sizeof(level)); level[source] = 0;
+  deque<int> queue; queue.push_back(source);
+  while (!queue.empty())
+  {
+    int u = queue.front(); queue.pop_front();
+    for (int i = 0; i < graph[u][maxVertices]; i ++)
+    {
+      int v = graph[u][i];
+      if (level[v] < 0 && matrixGraph[u][v] > 0)
+      {
+        level[v] = level[u] + 1;
+        queue.push_back(v);
+      }
+    }
+  }
+  return(level[target] > 0);
+}
+
+int dfs(int **graph, int u, int target, int flow)
+{
+  if (u == target) return(flow);
+  int actualFlow = 0;
+  for (int i = 0; i < graph[u][maxVertices]; i ++)
+  {
+    int v = graph[u][i];
+    if (level[u] + 1 == level[v] && matrixGraph[u][v] > 0)
+    {
+      int pathFlow = dfs(graph, v, target, min(flow, matrixGraph[u][v]));
+      flow -= pathFlow;
+      matrixGraph[u][v] -= pathFlow;
+      matrixGraph[v][u] += pathFlow;
+      actualFlow += pathFlow;
+    }
+  }
+  return(actualFlow);
+}
+
+int dinic(int **graph, int source, int target)
+{
+  int maxFlow = 0, pathFlow;
+  while (bfs(graph, source, target))
+    do
+    {
+      pathFlow = dfs(graph, source, target, inf);
+      maxFlow += pathFlow;
+    }
+    while (pathFlow);
+  return(maxFlow);
+}
+
+void addEdge(int **graph, int u, int v, int c)
+{
+  graph[u][graph[u][maxVertices]] = v; graph[u][maxVertices] ++;
+  matrixGraph[u][v] = c;
+  graph[v][graph[v][maxVertices]] = u; graph[v][maxVertices] ++;
+  matrixGraph[v][u] = 0;
+}
+
+int main()
+{
+  matrixGraph = (int**) malloc(maxVertices * sizeof(int*));
+  for (int i = 0; i < maxVertices; i ++)
+    matrixGraph[i] = (int*) malloc(maxVertices * sizeof(int));
+
+  int **graph = (int**) malloc(maxVertices * sizeof(int*));
+  for (int i = 0; i < maxVertices; i ++)
+    graph[i] = (int*) malloc((maxVertices + 1) * sizeof(int));
+
+  int monkeys, canDrink, test = 1;
+  while (scanf("%d %d", &monkeys, &canDrink) != EOF && !(monkeys == 0))
+  {
+    for (int i = 0; i < maxVertices; i ++)
+    {
+      graph[i][maxVertices] = 0;
+      memset(matrixGraph[i], 0, sizeof(int) * maxVertices);
+    }
+
+    int toDrink = 0, monkey[monkeys][3];
+    set<int> intervals;
+    for (int i = 0; i < monkeys; i ++)
+    {
+      int v, a, b; scanf("%d %d %d", &v, &a, &b);
+      intervals.insert(a); intervals.insert(b);
+      monkey[i][0] = a; monkey[i][1] = b; monkey[i][2] = v;
+      addEdge(graph, 0, i + 1, v);
+      toDrink += v;
+    }
+
+    int intervalsSize = intervals.size(), intervalsArray[intervals.size()], at = 0;
+    for (auto i: intervals) intervalsArray[at ++] = i;
+    sort(intervalsArray, intervalsArray + intervalsSize);
+
+    int groups[intervalsSize - 1][2];
+    for (int i = 0; i < intervalsSize - 1; i ++)
+    {
+      groups[i][0] = intervalsArray[i];
+      groups[i][1] = intervalsArray[i + 1];
+      DEBUG printf("%d -> (%d, %d)\n", i, groups[i][0], groups[i][1]);
+      addEdge(graph, i + 100 + 1, maxVertices - 1, (groups[i][1] - groups[i][0]) * canDrink);
+    }
+
+    for (int i = 0; i < monkeys; i ++)
+      for (int j = 0; j < intervalsSize - 1 && monkey[i][1] > groups[j][0]; j ++)
+        if (groups[j][0] >= monkey[i][0])
+        {
+          //printf("%d -> %d\n", i, j);
+          addEdge(graph, i + 1, j + 100 + 1, (groups[j][1] - groups[j][0]));
+        }
+
+    int maxFlow = dinic(graph, 0, maxVertices - 1);
+
+    printf("Case %d: %s\n", test ++, maxFlow == toDrink ? "Yes" : "No");
+    if (maxFlow == toDrink)
+      for (int i = 0; i < monkeys; i ++)
+      {
+        //printf("Monkey: %d (Had: %d) -> Group: %d (%d) (%d, %d)\n", i, monkey[i][2], j, matrixGraph[j + 100 + 1][i + 1], groups[j][0], groups[j][1]);
+        int drink[maxVertices][2], at = 0;
+        for (int j = 0; j < intervalsSize - 1; j ++)
+          if (matrixGraph[j + 100 + 1][i + 1])
+          {
+            if (at > 0 && groups[j][0] == drink[at - 1][1])
+              drink[at - 1][1] = groups[j][1];
+            else
+            {
+              drink[at][0] = groups[j][0];
+              drink[at ++][1] = groups[j][1];
+            }
+          }
+        printf("%d", at);
+        for (int j = 0; j < at; j ++)
+          printf(" (%d,%d)", drink[j][0], drink[j][1]);
+        printf("\n");
+      }
+  }
+
+  return(0);
+}
