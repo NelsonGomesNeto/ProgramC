dcb5b69800e855fd44b24f45c3327a8ca79efaf9
Nelson Gomes Neto <ngn@ic.ufal.br>
2017-12-01 18:54:16 -0300

    YaY, done ^~^; It seems like this other one is the same, but we will have to make the (vertice - edge) trick

--- UVA/maxFlow/10480Sabotage.cpp
+++ UVA/maxFlow/10480Sabotage.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define DEBUG if(0)
+#define lli long long int
 using namespace std;
 const int maxVertices = 50 + 2;
 int matrixGraph[maxVertices][maxVertices], level[maxVertices], inf = 1<<20;
@@ -53,14 +55,42 @@ void addEdge(vector<int> graph[], int u, int v, int c)
   graph[u].push_back(v);
   matrixGraph[u][v] = c;
   graph[v].push_back(u);
-  //matrixGraph[v][u] = 0; // ??
+  matrixGraph[v][u] = c;
+}
+
+/* In order to find the Min Cut, we need to perform a Max Flow, and them run a BFS/DFS from source throught the edges with capacity > 0, and the cutted ones will be the ones that have capacity == 0 and are connected to a unreachable vertice */
+vector<pair<int, int> > findCuttedEdges(vector<int> graph[], int source, int vertices)
+{
+  int visited[maxVertices]; memset(visited, 0, sizeof(visited));
+  vector<pair<int, int> > cutted, trullyCutted;
+  deque<int> queue; queue.push_back(source);
+  while (!queue.empty())
+  {
+    int u = queue.front(); queue.pop_front();
+    if (visited[u]) continue;
+    visited[u] = 1;
+    for (auto v: graph[u])
+    {
+      if (matrixGraph[u][v] > 0)
+        queue.push_back(v);
+      else if (!visited[v])
+        cutted.push_back({u, v});
+    }
+  }
+  for (int i = 0; i < cutted.size(); i ++)
+    if (!visited[cutted[i].second])
+      trullyCutted.push_back({cutted[i].first, cutted[i].second});
+    //if (visited[cutted[i].second]) cutted.erase(cutted.begin() + i);
+  return(trullyCutted);
 }
 
 int main()
 {
-  int cities, connections;
+  int cities, connections, run = 0;
   while (scanf("%d %d", &cities, &connections) && !(cities == 0 && connections == 0))
   {
+    if (run ++ > 0) printf("\n");
+
     memset(matrixGraph, 0, sizeof(matrixGraph));
     vector<int> graph[maxVertices];
     int u, v, c;
@@ -71,14 +101,19 @@ int main()
     }
 
     int maxFlow = dinic(graph, 1, 2);
-    printf("%d\n", maxFlow);
-    for (int i = 1; i <= cities; i ++)
-    {
-      printf("%d", i);
-      for (auto j: graph[i])
-        printf(" -> (%d, %d)", j, matrixGraph[i][j]);
-      printf("\n");
+    DEBUG {
+      printf("%d\n", maxFlow);
+      for (int i = 1; i <= cities; i ++)
+      {
+        printf("%d", i);
+        for (auto j: graph[i]) printf(" -> (%d, %d)", j, matrixGraph[i][j]);
+        printf("\n");
+      }
     }
+
+    vector<pair<int, int> > cutted = findCuttedEdges(graph, 1, cities);
+    for (auto i: cutted)
+      printf("%d %d\n", i.first, i.second);
   }
   return(0);
 }
