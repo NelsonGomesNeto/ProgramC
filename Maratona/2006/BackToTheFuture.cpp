fb203eb3d6b796eca04e75101537fb04a2fd3ba8
NelsonGomesNeto <ngn@ic.ufal.br>
2017-12-05 17:06:26 -0300

    LOL, not even that hahaha

--- Maratona/2006/BackToTheFuture.cpp
+++ Maratona/2006/BackToTheFuture.cpp
@@ -1,99 +1,109 @@
 #include <bits/stdc++.h>
-#define lli long long int
 using namespace std;
-lli inf = 1e16;
-int cities, routes, friends, freeSeats;
-int matrixGraph[101][101][2];
+const int maxVertices = 1000 + 2;
+int matrixGraph[maxVertices][maxVertices][2], inf = 1<<20, friends, capacity, vertices;
 
-int dijkstra(vector<pair<int, int> > graph[], int source, int target, int parent[])
+void printGraph(vector<int> graph[])
 {
-  int visited[cities]; memset(visited, 0, sizeof(visited));
-  int cost[cities];
-  for (int i = 0; i < cities; i ++) cost[i] = inf;
-  priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
-  pq.push({source, 0});
-  parent[source] = -1;
-  while (!pq.empty())
+  for (int i = 0; i < vertices; i ++)
   {
-    int u = pq.top().first, c = pq.top().second; pq.pop();
-    for (auto v: graph[u])
-      if (!visited[v.first] && matrixGraph[u][v.first][1] > 0 && matrixGraph[u][v.first][0] + c < cost[u])
-      {
-        cost[u] = matrixGraph[u][v.first][0] + c;
-        pq.push({v.first, matrixGraph[u][v.first][0] + c});
-        //queue.push_back(v.first);
-        parent[v.first] = u;
-        visited[v.first] = 1;
-      }
+    printf("%d", i);
+    for (auto j: graph[i])
+      printf(" -> (%d, %d, %d)", j, matrixGraph[i][j][0], matrixGraph[i][j][1]);
+    printf("\n");
   }
-  return(visited[target]);
 }
 
-lli fordFulkerson(vector<pair<int, int> > graph[], int source, int target)
+int bellmanFord(vector<int> graph[], int source, int target, int parent[])
 {
-  lli maxFlow = 0, cost = 0;
-  int parent[cities]; memset(parent, 0, sizeof(parent));
-  while (dijkstra(graph, source, target, parent))
+  int cost[vertices];
+  for (int i = 0; i < vertices; i ++) cost[i] = inf;
+  cost[source] = 0;
+
+  for (int i = 0; i < vertices - 1; i ++)
+    for (int u = 0; u < vertices; u ++)
+      for (auto v: graph[u])
+        if (cost[u] + matrixGraph[u][v][0] < cost[v] && matrixGraph[u][v][1] > 0)
+        {
+          cost[v] = cost[u] + matrixGraph[u][v][0];
+          parent[v] = u;
+        }
+
+  for (int u = 0; u < vertices; u ++)
+    for (auto v: graph[u])
+      if (cost[u] + matrixGraph[u][v][0] < cost[v] && matrixGraph[u][v][1] > 0)
+        return(0);
+
+  //printf("cost: %d\n", cost[target]);
+  return(cost[target] != inf);
+}
+
+int minMax(vector<int> graph[], int source, int target)
+{
+  int maxFlow = 0, cost = 0, parent[maxVertices]; memset(parent, -1, sizeof(parent));
+  // printGraph(graph);
+  while (bellmanFord(graph, source, target, parent))
   {
-    lli pathFlow = inf;
-    int v = target, u;
+    int pathFlow = inf, v = target, u;
     while (v != source)
     {
-      printf("%d %d\n", v, parent[v]);
+      //printf("%d ", v + 1);
       u = parent[v];
-      pathFlow = pathFlow < matrixGraph[u][v][1] ? pathFlow : matrixGraph[u][v][1];
+      pathFlow = min(pathFlow, matrixGraph[u][v][1]);
       v = parent[v];
     }
+    //printf("\npathFlow: %d\n", pathFlow);
+    pathFlow = capacity;
     v = target;
     while (v != source)
     {
       u = parent[v];
-      matrixGraph[u][v][1] = 0;
+      matrixGraph[u][v][1] -= pathFlow;
       matrixGraph[v][u][1] += pathFlow;
-      cost += matrixGraph[u][v][0];
+      matrixGraph[v][u][0] = matrixGraph[u][v][0] * -1;
+      cost += matrixGraph[u][v][0] * min(pathFlow, friends);
       v = parent[v];
     }
-    maxFlow += pathFlow;
-    if (maxFlow >= friends) return(cost);
+    maxFlow += min(pathFlow, friends);
+    //if (maxFlow == friends) break;
+    //printf("maxFlow: %d\n", maxFlow);
   }
+  if (maxFlow != friends) cost = inf;
+  return(cost);
+}
 
-  return(inf);
+void addEdge(vector<int> graph[], int u, int v, int dist, int cap)
+{
+  graph[u].push_back(v);
+  matrixGraph[u][v][0] = dist;
+  matrixGraph[u][v][1] = cap;
+  graph[v].push_back(u);
+  matrixGraph[v][u][0] = dist;
+  matrixGraph[v][u][1] = cap;
 }
 
 int main()
 {
-  int run = 1;
-  while (scanf("%d %d", &cities, &routes) != EOF)
+  int cities, flights, run = 1;
+  while (scanf("%d %d", &cities, &flights) != EOF)
   {
-    memset(matrixGraph, 0, sizeof(matrixGraph));
-    vector<pair<int, int> > graph[cities];
-    int u, v, c;
-    for (int i = 0; i < routes; i ++)
+    vertices = cities; memset(matrixGraph, 0, sizeof(matrixGraph));
+    vector<int> graph[maxVertices];
+    int u, v, c, input[flights][3];
+    for (int i = 0; i < flights; i ++)
     {
       scanf("%d %d %d", &u, &v, &c); u --; v --;
-      graph[u].push_back({v, c});
-      graph[v].push_back({u, c});
-      matrixGraph[u][v][0] = c; matrixGraph[v][u][0] = c;
+      input[i][0] = u; input[i][1] = v; input[i][2] = c;
     }
-    scanf("%d %d", &friends, &freeSeats);
+    scanf("%d %d", &friends, &capacity);
+    for (int i = 0; i < flights; i ++)
+      addEdge(graph, input[i][0], input[i][1], input[i][2], capacity);
 
-    for (int i = 0; i < cities; i ++)
-    {
-      for (auto j: graph[i])
-      {
-        matrixGraph[i][j.first][1] = freeSeats;
-        matrixGraph[j.first][i][1] = freeSeats;
-      }
-    }
-
-
-    lli ans = fordFulkerson(graph, 0, cities - 1);
-
+    int maxFlow = minMax(graph, 0, cities - 1);
+    if (run > 1) printf("\n");
     printf("Instancia %d\n", run ++);
-    if (ans != inf)
-      printf("%lld\n", ans * friends);
-    else
-      printf("impossivel\n");
+    if (maxFlow == inf) printf("impossivel\n");
+    else printf("%d\n", maxFlow);
   }
 
 
