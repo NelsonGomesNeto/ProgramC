d5e67076b63e88c3fc103b4323de62f38a99669d
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-01-18 19:35:17 -0300

    Finished 2nd Route

--- RotaUFMG/Roteiro 1/ColoqueTijolosNaParede.cpp
+++ RotaUFMG/Roteiro 1/ColoqueTijolosNaParede.cpp
@@ -9,10 +9,11 @@ void printRes(int res[9][9])
 
 int main()
 {
-  int n, prev[5][5], res[9][9]; memset(res, -1, sizeof(res));
+  int n, prev[5][5], res[9][9];
   scanf("%d", &n);
   while (n -- > 0)
   {
+    memset(res, -1, sizeof(res));
     for (int i = 0; i < 5; i ++)
       for (int j = 0; j <= i; j ++)
       {
@@ -20,23 +21,17 @@ int main()
         res[2*i][2*j] = prev[i][j];
       }
 
+    //printRes(res);
+
+    for (int i = 5 - 1; i >= 0 + 1; i --)
+      for (int j = 0; j <= i - 1; j ++)
+        res[2*i][2*j + 1] = (prev[i - 1][j] - (prev[i][j] + prev[i][j + 1])) / 2;
+
+    for (int i = 9 - 1; i >= 0 + 1; i --)
+      for (int j = 0; j <= i - 1; j ++)
+        res[i - 1][j] = res[i][j] + res[i][j + 1];
+
     printRes(res);
-
-    int missing = 31;
-    while (missing > 30)
-    {
-      for (int i = 5 - 1; i >= 0 + 1; i --)
-        for (int j = 0; j <= i - 1; j ++)
-          if (prev[i][j] + prev[i][j + 1] == prev[i - 1][j])
-          {
-            res[2*i][2*j] = prev[i][j];
-            res[2*i][2*j + 1] = 0;
-            res[2*i][2*j + 2] = prev[i][j];
-          }
-
-      missing --;
-      printRes(res);
-    }
   }
   return(0);
 }
