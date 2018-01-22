01c2ce2832453711f5ac630720e7760375c36614
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-01-21 22:23:18 -0300

    Holly, just one more...

---
+++ RotaUFMG/Roteiro 2/Mario.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  int n, l;
+  while (scanf("%d %d", &n, &l) && !(!n && !l))
+  {
+    int spaces[l];
+    for (int i = 0; i < spaces; i ++)
+      scanf("%d", &spaces[i]);
+
+
+  }
+  return(0);
+}
