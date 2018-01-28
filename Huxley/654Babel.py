60991a3e92c5a2a4cca91590953ddafbd8372927
NelsonGomesNeto <ngn@ic.ufal.br>
2018-01-27 21:16:47 -0300

    You've got to use Dijkstra with states!

--- Huxley/654Babel.py
+++ Huxley/654Babel.py
@@ -2,55 +2,36 @@ from heapq import *
 inf = 2**33
 DEBUG = 0
 
-def dijkstra(graph, cost, visited, start):
-    cost[start] = 0
+def dijkstra(graph, cost, start):
+    cost[origin] = [0] * 255
     pq = []
-    heappush(pq, [cost[start], start])
+    heappush(pq, [cost[start][0], start, '\0'])
     while (pq):
-        c, v = heappop(pq)
-        if (visited[v]): continue
-        visited[v] = 1
+        c, v, prev = heappop(pq)
         for u in graph[v]:
-            if (cost[v] + u[1] < cost[u[0]]):
-                cost[u[0]] = cost[v] + u[1]
-                heappush(pq, [cost[u[0]], u[0]])
+            if (u[2] != prev and c + u[1] < cost[u[0]][ord(u[2])]):
+                cost[u[0]][ord(u[2])] = c + u[1]
+                heappush(pq, [cost[u[0]][ord(u[2])], u[0], u[2]])
 
 while (True):
     words = int(input())
     if (words == 0): break
 
     origin, destination = input().split()
-
-    lines = [0] * words
-    stringToInt = {}
-    now = 1
-    stringToInt[origin] = 0
-    if (destination not in stringToInt):
-        stringToInt[destination] = 1
-        now = 2
+    graph = {}
+    graph[origin], graph[destination] = [], []
     for i in range(words):
-        line = list(input().split())
-        lines[i] = line
-        if (line[0] not in stringToInt):
-            stringToInt[line[0]] = now
-            now += 1
-        if (line[1] not in stringToInt):
-            stringToInt[line[1]] = now
-            now += 1
-    if (DEBUG):
-        print("stringToInt", stringToInt)
+        u, v, word = input().split()
+        if (u not in graph): graph[u] = []
+        if (v not in graph): graph[v] = []
+        graph[u] += [[v, len(word), word[0]]]
+        graph[v] += [[u, len(word), word[0]]]
 
-    graph = [[] for i in range(len(stringToInt))]
-    for i in lines:
-        graph[stringToInt[i[0]]] += [[stringToInt[i[1]], len(i[2])]]
-        graph[stringToInt[i[1]]] += [[stringToInt[i[0]], len(i[2])]]
-
-    if (DEBUG):
-        print("graph", graph)
-    cost, visited = [inf] * len(graph), [0] * len(graph)
-    dijkstra(graph, cost, visited, stringToInt[origin])
-
-    if (cost[stringToInt[destination]] == inf):
+    cost, visited = {}, {}
+    for i in graph:
+        cost[i] = [inf] * 255
+    dijkstra(graph, cost, origin)
+    if (min(cost[destination]) == inf):
         print("impossivel")
     else:
-        print(cost[stringToInt[destination]])
+        print(min(cost[destination]))
