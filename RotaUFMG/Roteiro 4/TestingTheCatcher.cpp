2bbc20f5b1f6e23f3f209ad80252246925149545
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-01-30 23:22:05 -0300

    These buggy problems...

---
+++ RotaUFMG/Roteiro 4/TestingTheCatcher.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  int first;
+  while (scanf("%d", &first) && first != -1)
+  {
+    int other, biggest = first, sequence = 0, higgest;
+    while (scanf("%d", &other) && other != -1)
+    {
+
+    }
+  }
+  return(0);
+}
