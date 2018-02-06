eea2590635c91e23a8aacce98e9a350db7cc270f
NelsonGomesNeto <ngn@ic.ufal.br>
2018-02-06 15:50:23 -0300

    Almosrt there...

--- RotaUFMG/Roteiro 4/ShellSort.cpp
+++ RotaUFMG/Roteiro 4/ShellSort.cpp
@@ -6,25 +6,30 @@ int main()
   int t; scanf("%d", &t);
   while (t --)
   {
-    int n; scanf("%d", &n);
+    int n; scanf("%d\n", &n);
     string un[n], so[n];
     for (int i = 0; i < n; i ++)
-      cin >> un[i];
+      getline(cin, un[i]);
     for (int i = 0; i < n; i ++)
-      cin >> so[i];
+      getline(cin, so[i]);
 
     set<string> toChange; int q = 0;
     for (int i = n - 1, j = n - 1; i >= q; i --, j --)
     {
+      //if (j >= 0) cout << so[i] << " | " << un[j] << endl;
       if (j < 0 || so[i] != un[j])
       {
-        q ++;
-        toChange.insert(un[j]);
-        for (j = n - 1; j >= 0; j --)
-          if (so[i] == un[j]) break;
+        if (j >= 0) q ++;
+        if (j >= 0) toChange.insert(un[j]);
+        i ++;
+        //for (j = n - 1 - q; j >= 0; j --)
+          //if (so[i] == un[j]) break;
       }
-      printf("%d\n", q);
     }
+
+    for (int i = n - 1; i >= 0; i --)
+      if (toChange.count(so[i]))
+        cout << so[i] << endl;
     printf("\n");
   }
   return(0);
