c033e53a153923004dfc60ad259cf37f4adb5d63
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-02-26 15:02:17 -0300

    LoL, 5 minutes do code this one hahaha, BUT TOOK ME DAYS TO THINK IT HAHAHA, it was so easy, I have no idea why I have not noticed it

--- RotaUFMG/Roteiro 5/IrEVir.cpp
+++ RotaUFMG/Roteiro 5/IrEVir.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
-#define DEBUG if(0)
 using namespace std;
-int hasPath[2000][2000];
 
-int dfs(vector<int> graph[], int u, int visited[], int target)
+int dfs(vector<int> graph[], int u, int visited[], int pathToAll[])
 {
+  if (pathToAll[u]) return(1);
   if (visited[u]) return(0);
-  if (hasPath[u][target]) return(1);
   visited[u] = 1;
   for (auto v: graph[u])
-  {
-    hasPath[u][v] = 1;
-    if (dfs(graph, v, visited, target))
-      hasPath[v][target] = hasPath[u][target] = 1;
-  }
-  return(hasPath[u][target]);
+    if (dfs(graph, v, visited, pathToAll))
+      return(1);
+  return(0);
 }
 
 int main()
@@ -22,30 +17,25 @@ int main()
   int n, m;
   while (scanf("%d %d", &n, &m) && n)
   {
-    memset(hasPath, 0, sizeof(hasPath));
     vector<int> graph[n]; int u, v, p;
     for (int i = 0; i < m; i ++)
     {
       scanf("%d %d %d", &u, &v, &p); u --; v --;
-      if (p == 2)
-      {
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-      }
-      else
-        graph[u].push_back(v);
+      graph[u].push_back(v);
+      if (p == 2) graph[v].push_back(u);
     }
 
-    int valid = 1, visited[n];
-    for (int i = 0; i < n; i ++)
-      for (int j = 0; j < n; j ++)
-      {
-        if (i != j)
-        {
-          memset(visited, 0, sizeof(visited));
-          valid = dfs(graph, i, visited, j);
-        }
-      }
+    int valid = 1, pathToAll[n], visited[n];
+    memset(pathToAll, 0, sizeof(pathToAll));
+    for (int i = 0; i < n && valid; i ++)
+    {
+      memset(visited, 0, sizeof(visited));
+      if (!dfs(graph, i, visited, pathToAll))
+        for (int j = 0; j < n; j ++)
+          if (!visited[j]) valid = 0;
+      pathToAll[i] = 1;
+    }
+
     printf("%d\n", valid);
   }
   return(0);
