23c5acfd1471356203dc317f4afb19c11113f2c0
NelsonGomesNeto <ngn@ic.ufal.br>
2018-02-28 12:41:29 -0300

    LoL, such a silly mistake

--- CodeForces/1ATheatreSquare.cpp
+++ CodeForces/1ATheatreSquare.cpp
@@ -4,6 +4,6 @@
 int main()
 {
   lli n, m, a; scanf("%lld %lld %lld", &n, &m, &a);
-  printf("%lld\n", (((n % a) > 0) + (n / a)) + (((m % a) > 0) + (m / a)));
+  printf("%lld\n", (((n % a) > 0) + (n / a)) * (((m % a) > 0) + (m / a)));
   return(0);
 }
