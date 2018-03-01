04f2f43351464fe9cb1781094123354452164028
NelsonGomesNeto <ngn@ic.ufal.br>
2018-03-01 10:38:14 -0300

    ...

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-while (True):
-    try:
-        a = int(input())
-    except EOFError as e:
-        break
-
-
-s = "HArdwArE"
-j = 0
-for i in s:
-    print("addi $t0, $zero, %d # t0 = %c" % (ord(i), i))
-    print("sb $t0, 0($t1) # s[%d] = t0" % (j))
-    print("addi $t1, $t1, 4 # t += 4 (i += 1)")
-    j += 1
-print("sb $zero, 0($t1) # s[%d] = '\0'" % (j))
+import sys
+print(sys.argv)
+print("sum =", int(sys.argv[1]) + int(sys.argv[2]))
