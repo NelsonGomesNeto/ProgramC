3f9cd25bd120b7ad6742574b942e0a52c95ad3ab
NelsonGomesNeto <ngn@ic.ufal.br>
2018-04-15 17:30:48 -0300

    I'm so dumb

--- test.c
+++ test.c
@@ -1,44 +1,8 @@
 #include <stdio.h>
-
-void read(int array[], int size)
-{
-  if (size == 0) return;
-  read(array, size - 1);
-  scanf("%d", &array[size - 1]);
-}
-
-void print(int array[], int size)
-{
-  if (size == 0) return;
-  printf("%d%c", array[size - 1], size - 1 ? ' ' : '\n');
-  print(array, size - 1);
-}
-
-void swap(int *a, int *b)
-{
-  int aux = *a;
-  *a = *b;
-  *b = aux;
-}
-
-void sort(int array[], int i, int j, int size)
-{
-  if (i >= size) return;
-  if (j >= size) return(sort(array, i + 1, i + 2, size));
-  if (array[i] < array[j]) swap(&array[i], &array[j]);
-  sort(array, i, j + 1, size);
-}
+#include <iostream>
 
 int main()
 {
-  int size; scanf("%d", &size);
-  int array[size];
-  read(array, size);
-  print(array, size);
-
-  sort(array, 0, 0, size);
-
-  print(array, size);
-
+  std::cout << "YaY" << std::endl;
   return(0);
 }
