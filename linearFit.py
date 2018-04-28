9865251baa976342488490be791a4bd40217c4e0
NelsonGomesNeto <ngn@ic.ufal.br>
2018-04-28 11:10:26 -0300

    Nice

--- linearFit.py
+++ linearFit.py
@@ -1,11 +1,15 @@
 import sys
-sys.setrecursionlimit(2**20)
+from math import tan, acos
+sys.setrecursionlimit(2**19)
+
+def deg2rad(coef):
+    return(coef * acos(-1) / 180)
 
 def eva(x, y, coef, b):
     error = 0
-    for i in range(20):
-        error += (y[i] - (x[i] * coef + b))**2
-    return(error / 20)
+    for i in range(19):
+        error += (y[i] - tan(deg2rad(x[i] * coef + b)))**2
+    return(error / 19)
 
 def loleva(x, y, b, wut):
     bestCoef = ternarySearch(0, 100000, x, y, eva, b)
@@ -19,11 +23,12 @@ def ternarySearch(lo, hi, x, y, func, b):
     return(ternarySearch(midlo, hi, x, y, func, b))
 
 x, y = [], []
-for i in range(20):
+for i in range(19):
     x += [float(input())]
-for i in range(20):
+for i in range(19):
     y += [float(input())]
 
-b = ternarySearch(0, 100000, x, y, loleva, 0)
-a = ternarySearch(0, 100000, x, y, eva, b)
-print(a, b, "error:", eva(x, y, a, b))
+#b = ternarySearch(0, 100000, x, y, loleva, 0)
+a = ternarySearch(0, 100000, x, y, eva, 0)
+#print(a, b, "error:", eva(x, y, a, b))
+print(a, eva(x, y, a, 0))
