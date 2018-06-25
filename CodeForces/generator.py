de50465d5ad9167fff445a8cca583a02db5ed58e
NelsonGomesNeto <root@NELSON-AMD.ngomesneto>
2018-06-25 19:34:44 -0300

    Nice, it would have been a successful contest hahaha

--- CodeForces/generator.py
+++ CodeForces/generator.py
@@ -1,15 +1,9 @@
-from random import random, randint
+from random import random, randint, shuffle
 
-n, m = 200000, 2
+n = 100
 a = []
-now = 199999
-for i in range(n):
-  # a += [now]
-  # if (i > 0 and not (i & 1)):
-  #   now -= 1
-  # a += [randint(0, 10**9)]
-  a += [1]
-f = open("gin", "w")
-print(n, m, file=f)
-print(*a, file=f)
-f.close()
+a += list(range(1, n + 1))
+a += list(range(1, n + 1))
+shuffle(a)
+print(n)
+print(*a)
