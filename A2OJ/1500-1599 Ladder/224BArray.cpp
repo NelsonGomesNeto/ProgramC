4592d0fd368f6f3b07af7030ce5d8b257781f951
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-12 08:25:56 -0300

    Nice start

--- A2OJ/1500-1599 Ladder/224BArray.cpp
+++ A2OJ/1500-1599 Ladder/224BArray.cpp
@@ -6,33 +6,29 @@ int main()
 {
   int n, k; scanf("%d %d", &n, &k);
   int a[n]; for (int i = 0; i < n; i ++) scanf("%d", &a[i]);
-  int diff[n + 1]; diff[0] = 0; set<int> now;
+  set<int> now; int l = 0, r = -1, size = 0;
+  int app[100001]; memset(app, 0, sizeof(app));
   for (int i = 0; i < n; i ++)
   {
-    now.insert(a[i]);
-    diff[i + 1] = now.size();
-  }
-  DEBUG for (int i = 0; i <= n; i ++)
-    printf("%d %d\n", i, diff[i]);
-
-  if (diff[n] < k) printf("-1 -1\n");
-  else
-  {
-    int l = -1, r = -1, minSize = n + 1;
-    for (int i = 1; i <= n; i ++)
+    now.insert(a[i]); app[a[i]] ++;
+    if (now.size() == k)
     {
-      while (i + 1 <= n && diff[i] == diff[i + 1]) i ++;
-      int end = lower_bound(diff, diff+n+1, k + diff[i] - (i > 0)) - diff;
-      DEBUG if (end <= n) printf("%d %d %d %d\n", i, end, diff[i], diff[end]);
-      if (end >= i && end <= n && diff[end] - diff[i] + (i > 0) == k && end - i + 1 < minSize)
+      r = i;
+      DEBUG printf("%d %d\n", l, r);
+      while (l < n && now.size() == k)
       {
-        l = i;
-        r = end;
-        minSize = end - i + 1;
+        app[a[l]] --;
+        if (!app[a[l]]) now.erase(a[l]);
+        l ++;
       }
+      l = min(l, r);
+      DEBUG printf("%d %d\n", l, r);
     }
-    printf("%d %d\n", l, r);
   }
+
+  if (r == -1) printf("-1 -1\n");
+  else if (k == 1) printf("1 1\n");
+  else printf("%d %d\n", l, r + 1);
 
   return(0);
 }
