89790b03ed9b10d53e2d623bb669d7ded959ea47
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-13 21:50:40 -0300

    Implemented BFS solution for the Tilling Problem

--- PAA/divideAndConquer/tillingProblem/build/source/tillingProblem.java
+++ PAA/divideAndConquer/tillingProblem/build/source/tillingProblem.java
@@ -3,6 +3,9 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import java.util.Queue; 
+import java.util.ArrayDeque; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -14,9 +17,12 @@ import java.io.IOException;
 
 public class tillingProblem extends PApplet {
 
+
+
+
 int table[][] = new int[1024][1024];
 int now = 0;
-int size, y, x, biggest, waitTime = 10, nowli, nowlj, nowhi, nowhj; float blockSize;
+int size, y, x, biggest, waitTime = 1000, nowli, nowlj, nowhi, nowhj; float blockSize;
 int[] dci = {0, 0, 1, 1}, dcj = {1, 0, 0, 1};
 int[] done = new int[361];
 PGraphics pg;
@@ -30,27 +36,27 @@ public void setup() {
   table[y][x] = -1;
   
   blockSize = PApplet.parseFloat(min(height, width)) / size;
-  print(blockSize);
+  print(blockSize, "\n");
   colorMode(HSB, 360, 100, 100);
 
   pg = createGraphics(900, 900);
+  pg.beginDraw();
   pgDrawBlock(y, x);
+  pg.endDraw();
   thread("startSolve");
 }
 
 public void pgDrawBlock(int i, int j) {
-  pg.beginDraw();
   pg.colorMode(HSB, 360, 100, 100);
   if (table[i][j] == -1) pg.fill(0, 0, 0);
-  else pg.fill(360*(PApplet.parseFloat(table[i][j]) / biggest), 100, 100);
-  pg.rect(i * blockSize, j * blockSize, blockSize, blockSize);
-  pg.endDraw();
+  else pg.fill(300*(PApplet.parseFloat(table[i][j]) / (log(biggest)/log(4))), 100, 100);
+  pg.rect(j * blockSize, i * blockSize, blockSize, blockSize);
 }
 
 public void drawBlock(int i, int j) {
   if (table[i][j] == -1) fill(0, 0, 0);
   else fill(360*(PApplet.parseFloat(table[i][j]) / biggest), 100, 100);
-  rect(i * blockSize, j * blockSize, blockSize, blockSize);
+  rect(j * blockSize, i * blockSize, blockSize, blockSize);
 }
 
 public void draw() {
@@ -58,11 +64,6 @@ public void draw() {
   //   for (int j = nowlj; j < nowhj; j ++)
   //     drawBlock(i, j);
   image(pg, 0, 0);
-}
-
-public void startSolve() {
-  now = 0; for (int i = 0; i <= 360; i ++) done[i] = 0;
-  solve(0, 0, size - 1, size - 1, y, x);
 }
 
 public int getDir(int mi, int mj, int y, int x) {
@@ -74,34 +75,80 @@ public int getDir(int mi, int mj, int y, int x) {
 }
 
 public void putColor(int mi, int mj, int dir, int col) {
+  // pg.beginDraw();
   for (int i = 0; i < 2; i ++)
     for (int j = 0; j < 2; j ++) {
       if (dci[dir] == i && dcj[dir] == j) continue;
       table[mi + i][mj + j] = col;
       pgDrawBlock(mi + i, mj + j);
     }
+  // pg.endDraw();
   // nowli = mi; nowlj = mj; nowhi = mi + 2; nowhj = mj + 2;
 }
 
-public void solve(int li, int lj, int hi, int hj, int y, int x) {
+public void startSolve() {
+  now = 0; for (int i = 0; i <= 360; i ++) done[i] = 0;
+  // solveDFS(0, 0, size - 1, size - 1, y, x, 0);
+  solveBFS();
+}
+
+public void solveDFS(int li, int lj, int hi, int hj, int y, int x, int c) {
   if (li >= hi && lj >= hj) return;
-  now ++;
   int mi = (li + hi) / 2, mj = (lj + hj) / 2;
   int dir = getDir(mi, mj, y, x);
 
   // int newColor; do newColor = int(random(0, 360)); while (done[newColor] == 1);
   // done[newColor] = 1;
-  putColor(mi, mj, dir, now);
+  putColor(mi, mj, dir, c);
 
   delay(waitTime);
-  if (dir == 0) solve(li, mj + 1, mi, hj, y, x);
-  else solve(li, mj + 1, mi, hj, mi, mj + 1);
-  if (dir == 1) solve(li, lj, mi, mj, y, x);
-  else solve(li, lj, mi, mj, mi, mj);
-  if (dir == 2) solve(mi + 1, lj, hi, mj, y, x);
-  else solve(mi + 1, lj, hi, mj, mi + 1, mj);
-  if (dir == 3) solve(mi + 1, mj + 1, hi, hj, y, x);
-  else solve(mi + 1, mj + 1, hi, hj, mi + 1, mj + 1);
+  now ++;
+  if (dir == 0) solveDFS(li, mj + 1, mi, hj, y, x, c + 1);
+  else solveDFS(li, mj + 1, mi, hj, mi, mj + 1, c + 1);
+  if (dir == 1) solveDFS(li, lj, mi, mj, y, x, c + 1);
+  else solveDFS(li, lj, mi, mj, mi, mj, c + 1);
+  if (dir == 2) solveDFS(mi + 1, lj, hi, mj, y, x, c + 1);
+  else solveDFS(mi + 1, lj, hi, mj, mi + 1, mj, c + 1);
+  if (dir == 3) solveDFS(mi + 1, mj + 1, hi, hj, y, x, c + 1);
+  else solveDFS(mi + 1, mj + 1, hi, hj, mi + 1, mj + 1, c + 1);
+}
+
+public void solveBFS() {
+  Queue<int[]> queue = new ArrayDeque();
+  int[] aux = {0, 0, size - 1, size - 1, y, x, 0};
+  queue.add(aux.clone());
+  int level = -1; pg.beginDraw();
+  while (queue.size() > 0) {
+    int li, lj, hi, hj, yy, xx, c;
+    li = queue.peek()[0]; lj = queue.peek()[1]; hi = queue.peek()[2]; hj = queue.peek()[3]; yy = queue.peek()[4]; xx = queue.peek()[5]; c = queue.peek()[6]; queue.remove();
+    if (li >= hi && lj >= hj) continue;
+    if (c > level)
+    {
+      pg.endDraw();
+      delay(waitTime);
+      level = c;
+      pg.beginDraw();
+    }
+
+    int mi = (li + hi) / 2, mj = (lj + hj) / 2;
+    int dir = getDir(mi, mj, yy, xx);
+
+    putColor(mi, mj, dir, c);
+
+    if (dir == 0) aux = new int[] {li, mj + 1, mi, hj, yy, xx, c + 1};
+    else aux = new int[] {li, mj + 1, mi, hj, mi, mj + 1, c + 1};
+    queue.add(aux.clone());
+    if (dir == 1) aux = new int[] {li, lj, mi, mj, yy, xx, c + 1};
+    else aux = new int[] {li, lj, mi, mj, mi, mj, c + 1};
+    queue.add(aux.clone());
+    if (dir == 2) aux = new int[] {mi + 1, lj, hi, mj, yy, xx, c + 1};
+    else aux = new int[] {mi + 1, lj, hi, mj, mi + 1, mj, c + 1};
+    queue.add(aux.clone());
+    if (dir == 3) aux = new int[] {mi + 1, mj + 1, hi, hj, yy, xx, c + 1};
+    else aux = new int[] {mi + 1, mj + 1, hi, hj, mi + 1, mj + 1, c + 1};
+    queue.add(aux.clone());
+  }
+  pg.endDraw();
 }
   public void settings() {  size(900, 900); }
   static public void main(String[] passedArgs) {
--- PAA/divideAndConquer/tillingProblem/tillingProblem.pde
+++ PAA/divideAndConquer/tillingProblem/tillingProblem.pde
@@ -1,6 +1,9 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
 int table[][] = new int[1024][1024];
 int now = 0;
-int size, y, x, biggest, waitTime = 10, nowli, nowlj, nowhi, nowhj; float blockSize;
+int size, y, x, biggest, waitTime = 1000, nowli, nowlj, nowhi, nowhj; float blockSize;
 int[] dci = {0, 0, 1, 1}, dcj = {1, 0, 0, 1};
 int[] done = new int[361];
 PGraphics pg;
@@ -14,27 +17,27 @@ void setup() {
   table[y][x] = -1;
   size(900, 900);
   blockSize = float(min(height, width)) / size;
-  print(blockSize);
+  print(blockSize, "\n");
   colorMode(HSB, 360, 100, 100);
 
   pg = createGraphics(900, 900);
+  pg.beginDraw();
   pgDrawBlock(y, x);
+  pg.endDraw();
   thread("startSolve");
 }
 
 void pgDrawBlock(int i, int j) {
-  pg.beginDraw();
   pg.colorMode(HSB, 360, 100, 100);
   if (table[i][j] == -1) pg.fill(0, 0, 0);
-  else pg.fill(360*(float(table[i][j]) / biggest), 100, 100);
-  pg.rect(i * blockSize, j * blockSize, blockSize, blockSize);
-  pg.endDraw();
+  else pg.fill(300*(float(table[i][j]) / (log(biggest)/log(4))), 100, 100);
+  pg.rect(j * blockSize, i * blockSize, blockSize, blockSize);
 }
 
 void drawBlock(int i, int j) {
   if (table[i][j] == -1) fill(0, 0, 0);
   else fill(360*(float(table[i][j]) / biggest), 100, 100);
-  rect(i * blockSize, j * blockSize, blockSize, blockSize);
+  rect(j * blockSize, i * blockSize, blockSize, blockSize);
 }
 
 void draw() {
@@ -42,11 +45,6 @@ void draw() {
   //   for (int j = nowlj; j < nowhj; j ++)
   //     drawBlock(i, j);
   image(pg, 0, 0);
-}
-
-void startSolve() {
-  now = 0; for (int i = 0; i <= 360; i ++) done[i] = 0;
-  solve(0, 0, size - 1, size - 1, y, x);
 }
 
 int getDir(int mi, int mj, int y, int x) {
@@ -58,32 +56,78 @@ int getDir(int mi, int mj, int y, int x) {
 }
 
 void putColor(int mi, int mj, int dir, int col) {
+  // pg.beginDraw();
   for (int i = 0; i < 2; i ++)
     for (int j = 0; j < 2; j ++) {
       if (dci[dir] == i && dcj[dir] == j) continue;
       table[mi + i][mj + j] = col;
       pgDrawBlock(mi + i, mj + j);
     }
+  // pg.endDraw();
   // nowli = mi; nowlj = mj; nowhi = mi + 2; nowhj = mj + 2;
 }
 
-void solve(int li, int lj, int hi, int hj, int y, int x) {
+void startSolve() {
+  now = 0; for (int i = 0; i <= 360; i ++) done[i] = 0;
+  // solveDFS(0, 0, size - 1, size - 1, y, x, 0);
+  solveBFS();
+}
+
+void solveDFS(int li, int lj, int hi, int hj, int y, int x, int c) {
   if (li >= hi && lj >= hj) return;
-  now ++;
   int mi = (li + hi) / 2, mj = (lj + hj) / 2;
   int dir = getDir(mi, mj, y, x);
 
   // int newColor; do newColor = int(random(0, 360)); while (done[newColor] == 1);
   // done[newColor] = 1;
-  putColor(mi, mj, dir, now);
+  putColor(mi, mj, dir, c);
 
   delay(waitTime);
-  if (dir == 0) solve(li, mj + 1, mi, hj, y, x);
-  else solve(li, mj + 1, mi, hj, mi, mj + 1);
-  if (dir == 1) solve(li, lj, mi, mj, y, x);
-  else solve(li, lj, mi, mj, mi, mj);
-  if (dir == 2) solve(mi + 1, lj, hi, mj, y, x);
-  else solve(mi + 1, lj, hi, mj, mi + 1, mj);
-  if (dir == 3) solve(mi + 1, mj + 1, hi, hj, y, x);
-  else solve(mi + 1, mj + 1, hi, hj, mi + 1, mj + 1);
+  now ++;
+  if (dir == 0) solveDFS(li, mj + 1, mi, hj, y, x, c + 1);
+  else solveDFS(li, mj + 1, mi, hj, mi, mj + 1, c + 1);
+  if (dir == 1) solveDFS(li, lj, mi, mj, y, x, c + 1);
+  else solveDFS(li, lj, mi, mj, mi, mj, c + 1);
+  if (dir == 2) solveDFS(mi + 1, lj, hi, mj, y, x, c + 1);
+  else solveDFS(mi + 1, lj, hi, mj, mi + 1, mj, c + 1);
+  if (dir == 3) solveDFS(mi + 1, mj + 1, hi, hj, y, x, c + 1);
+  else solveDFS(mi + 1, mj + 1, hi, hj, mi + 1, mj + 1, c + 1);
+}
+
+void solveBFS() {
+  Queue<int[]> queue = new ArrayDeque();
+  int[] aux = {0, 0, size - 1, size - 1, y, x, 0};
+  queue.add(aux.clone());
+  int level = -1; pg.beginDraw();
+  while (queue.size() > 0) {
+    int li, lj, hi, hj, yy, xx, c;
+    li = queue.peek()[0]; lj = queue.peek()[1]; hi = queue.peek()[2]; hj = queue.peek()[3]; yy = queue.peek()[4]; xx = queue.peek()[5]; c = queue.peek()[6]; queue.remove();
+    if (li >= hi && lj >= hj) continue;
+    if (c > level)
+    {
+      pg.endDraw();
+      delay(waitTime);
+      level = c;
+      pg.beginDraw();
+    }
+
+    int mi = (li + hi) / 2, mj = (lj + hj) / 2;
+    int dir = getDir(mi, mj, yy, xx);
+
+    putColor(mi, mj, dir, c);
+
+    if (dir == 0) aux = new int[] {li, mj + 1, mi, hj, yy, xx, c + 1};
+    else aux = new int[] {li, mj + 1, mi, hj, mi, mj + 1, c + 1};
+    queue.add(aux.clone());
+    if (dir == 1) aux = new int[] {li, lj, mi, mj, yy, xx, c + 1};
+    else aux = new int[] {li, lj, mi, mj, mi, mj, c + 1};
+    queue.add(aux.clone());
+    if (dir == 2) aux = new int[] {mi + 1, lj, hi, mj, yy, xx, c + 1};
+    else aux = new int[] {mi + 1, lj, hi, mj, mi + 1, mj, c + 1};
+    queue.add(aux.clone());
+    if (dir == 3) aux = new int[] {mi + 1, mj + 1, hi, hj, yy, xx, c + 1};
+    else aux = new int[] {mi + 1, mj + 1, hi, hj, mi + 1, mj + 1, c + 1};
+    queue.add(aux.clone());
+  }
+  pg.endDraw();
 }
