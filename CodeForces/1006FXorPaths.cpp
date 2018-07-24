5cf312a69f11fcaf2bf3e4111225aae39a2b92f5
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-24 01:23:53 -0300

    FINALLY!!!!!!! HOLLY

--- CodeForces/1006FXorPaths.cpp
+++ CodeForces/1006FXorPaths.cpp
@@ -1,43 +1,44 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define DEBUG if(0)
 #define lli long long int
-lli grid[20][20], n, m, k, lol[20][20], dp[20][20], dpRes;
+lli grid[20][20], n, m, middle, k;
+unordered_map<lli, lli> bot[20][20], dp[20][20];
 
-lli allPaths(int i, int j, lli prev, int depth)
+void build(int i, int j, lli prev)
 {
-  if (i >= n || j >= m) return(0);
-  DEBUG for (int k = 0; k < depth; k ++) printf("\t"); DEBUG printf("(%d, %d [%lld][%lld])\n", i, j, grid[i][j], prev ^ grid[i][j]);
-  if (i == n - 1 && j == m - 1) return((prev ^ grid[i][j]) == k);
+  if (i >= n || j >= m) return;
+  lli next = prev ^ grid[i][j];
+  if (i + j == middle)
+  {
+    if (!bot[i][j].count(next)) bot[i][j][next] = 0;
+    bot[i][j][next] ++;
+    return;
+  }
+  build(i + 1, j, next);
+  build(i, j + 1, next);
+}
 
-  if (lol[i][j] && prev == dp[i][j])
-  {
-    dpRes += lol[i][j] + 1;
-    lol[i][j] ++;
-    return(1);
-  }
-
-  if (allPaths(i + 1, j, prev ^ grid[i][j], depth + 1))
-  {
-    dp[i][j] = prev;
-    lol[i][j] ++;
-  }
-  if (allPaths(i, j + 1, prev ^ grid[i][j], depth + 1))
-  {
-    dp[i][j] = prev;
-    lol[i][j] ++;
-  }
-  return(lol[i][j] > 0);
+lli backPath(int i, int j, lli prev)
+{
+  if (i < 0 || j < 0) return(0);
+  if (i + j == middle) return(bot[i][j].count(prev ^ k) ? bot[i][j][prev ^ k] : 0);
+  lli next = prev ^ grid[i][j];
+  if (!dp[i][j].count(prev))
+    dp[i][j][prev] = backPath(i - 1, j, next) + backPath(i, j - 1, next);
+  return(dp[i][j][prev]);
 }
 
 int main()
 {
   scanf("%lld %lld %lld", &n, &m, &k);
-   for (int i = 0; i < n; i ++) for (int j = 0; j < m; j ++) scanf("%lld", &grid[i][j]);
+  for (int i = 0; i < n; i ++) for (int j = 0; j < m; j ++) scanf("%lld", &grid[i][j]);
 
-  memset(lol, 0, sizeof(lol)); dpRes = 0;
-  allPaths(0, 0, 0, 0);
-  DEBUG for (int i = 0; i < n; i ++) for (int j = 0; j < m; j ++) printf("%lld%c", lol[i][j], j < m - 1 ? ' ' : '\n');
-  printf("%lld\n", lol[n - 2][m - 1] + lol[n - 1][m - 2] + dpRes);
+  middle = (n + m) / 2 - 1;
+  build(0, 0, 0);
+  lli ans = backPath(n - 1, m - 1, 0);
+
+  printf("%lld\n", ans);
 
   return(0);
 }
