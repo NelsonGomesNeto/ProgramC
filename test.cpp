ca684cc25d397934cbc158383a9eaea3ecd8d28c
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-29 01:08:16 -0300

    Running out of time, I will have to try it later

--- test.cpp
+++ test.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 
+void swap(int *a, int *b)
+{
+  int aux = *a;
+  *a = *b;
+  *b = aux;
+}
+
 int main()
 {
-  char line[10000], c;
-  // for (int i = 0; i < 403; i ++)
-  // {
-  //   scanf("%c", &c);
-  //   printf("%d - <%c>\n", i, c);
-  // }
-  while (scanf("%[^\n]", line) != EOF)
-  {
-    printf("%s\n", line);
-    getchar();
-  }
+  int a = 0, b = 1;
+  swap(&a, &b);
+  printf("%d %d\n", a, b);
   return(0);
 }
