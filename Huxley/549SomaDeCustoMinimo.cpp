162aafb73279b2e4d3a06514aa154e928c775d48
NelsonGomesNeto <ngn@ic.ufal.br>
2018-08-23 00:49:01 -0300

    YaY, did a VERY interesting problem of Dijkstra and Ternary Search

---
+++ Huxley/549SomaDeCustoMinimo.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+
+int main()
+{
+  int n, seed, a, b, c;
+  while (scanf("%d %d %d %d %d", &n, &seed, &a, &b, &c) != EOF)
+  {
+    // lli array[n]; array[0] = seed;
+    // for (int i = 1; i < n; i ++) array[i] = (array[i - 1] * a + b) % c;
+
+    priority_queue<lli, vector<lli>, greater<lli> > pq;
+    pq.push(seed); lli prev = seed;
+    for (int i = 0; i < n; i ++)
+    {
+      lli next = (prev * a + b) % c;
+      pq.push(next);
+      prev = next;
+    }
+
+    lli sum = 0, cost = 0;
+    lli n1, n2;
+    while (!pq.empty())
+    {
+      n1 = n2 = 0;
+      n1 = pq.top(); pq.pop();
+      if (!pq.empty())
+      {
+        n2 = pq.top(); pq.pop();
+      }
+      cost += n1 + n2;
+      sum = n1 + n2;
+      if (!pq.empty()) pq.push(n1 + n2);
+    }
+    printf("%lld %lld\n", sum, cost);
+  }
+  return(0);
+}
