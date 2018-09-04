631643221b467aeea1e97accb67616f72e57a1d1
NelsonGomesNeto <ngn@ic.ufal.br>
2018-09-04 08:07:38 -0300

    FINALLY

--- Huxley/683DFSImprimindoAsFolhas.py
+++ Huxley/683DFSImprimindoAsFolhas.py
@@ -1,43 +1,32 @@
-def floodFill(graph, visited, leafs, root, level):
-    if (visited[root]): return
-    visited[root] = 1
-    leafs[level] += [root]
-    if (len(graph[root]) == 0): return
-    for u in graph[root]:
-        floodFill(graph, visited, leafs, u, level + 1)
+def dfs(graph, u, leafs):
+    if (len(graph[u]) == 0): leafs.add(u)
+    for v in graph[u]:
+        dfs(graph, v, leafs)
 
 while (True):
     try:
         lines = int(input())
     except:
         break
-    graph, first = {}, -1
+    graph = {}
     for i in range(lines):
         u, v = input().split()
-        if (first == -1): first = u
-        if (u not in graph):
-            graph[u] = []
-        if (v not in graph):
-            graph[v] = []
+        if (u not in graph): graph[u] = []
+        if (v not in graph): graph[v] = []
         graph[u] += [v]
-    print("graph", graph)
 
-    leafs = [[] for i in range(len(graph) + 1)]
-    visited = {}
-    for i in graph:
-        visited[i] = 0
-    for i in graph:
-        if (not visited[i]):
-            floodFill(graph, visited, leafs, i, 0)
-            print(visited)
-
-    allLeafs = []
-    for i in range(len(leafs)):
-        leafs[len(leafs) - i - 1].sort()
-        allLeafs += leafs[len(leafs) - i - 1]
-    print("allLeafs", allLeafs)
-    # for i in leafs:
-    #     if (len(i) == 0):
-    #         break
-    #     for j in i:
-    #         print(j)
+    allLeafs, done = [], 0
+    while (len(graph)):
+        leafs = set()
+        for i in graph:
+            dfs(graph, i, leafs)
+        for l in leafs:
+            for i in graph:
+                if (l in graph[i]):
+                    graph[i].remove(l)
+            del graph[l]
+        allLeafs += [sorted(leafs)]
+        done += len(leafs)
+    for l in allLeafs:
+        for i in l:
+            print(i)
