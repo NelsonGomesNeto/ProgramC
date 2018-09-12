bb2d26de53b8c4efefae631ac1ae8fa374ab7d71
NelsonGomesNeto <ngn@ic.ufal.br>
2018-09-12 09:22:57 -0300

    What a nice problem for P1 kids

---
+++ My Problems/Moving dx dy/moving.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+int n, m, dx, dy;
+
+void printMat(int mat[][m])
+{
+  int i, j;
+  for (i = 0; i < n; i ++)
+    for (j = 0; j < m; j ++)
+      printf("%3d%c", mat[i][j], j < m - 1 ? ' ' : '\n');
+}
+
+int fix(int i, int size)
+{
+  if (i < 0) return(i % size + size);
+  return(i % size);
+}
+
+void move(int i, int j, int mat[][m])
+{
+  int step = 1;
+  while (!mat[i][j])
+  {
+    mat[i][j] = step ++;
+    i = fix(i + dy, n);
+    j = fix(j + dx, m);
+  }
+}
+
+int main()
+{
+  scanf("%d %d", &n, &m);
+  int mat[n][m]; memset(mat, 0, sizeof(mat));
+  scanf("%d %d", &dx, &dy);
+
+  move(0, 0, mat);
+  printMat(mat);
+}
