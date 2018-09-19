ac578a54b6ec41e0c4426dc75032d80487a8e392
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-09-19 08:09:50 -0300

    YaY, B from 2017 Regional FINALLY DONE, it was pretty straight forward if you knew math a bit

--- Maratona/2017/Regionals/B (Brincadeira)/B.cpp
+++ Maratona/2017/Regionals/B (Brincadeira)/B.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define DEBUG if(0)
 #define lli long long int
-<<<<<<< HEAD
 int n, t, tap[31];
 lli now, previ, a0, x, y;
 
@@ -30,57 +29,13 @@ pair<int, int> solve()
     else
       firstApp[sum] = i + 1;
   }
-=======
-int n, t, a0, x, y, tap[31], inf = 1<<16;
-vector<lli> sequence;
-
-void generateSequence()
-{
-  lli now = a0;
-  while (true)
-  {
-    int bit = now & 1;
-    for (int i = 1; i < t; i ++) bit ^= (now >> tap[i]) & 1;
-    now = (now >> 1) | (bit << (n - 1));
-    if (now != sequence[0]) sequence.push_back(now);
-    else break;
-  }
-  DEBUG for (int i = 0; i < sequence.size(); i ++)
-    printf("%lld%c", sequence[i], i < sequence.size() - 1 ? ' ' : '\n');
->>>>>>> 56eb7b497b6b3122088c1ce44e156402209ef6fa
-}
-
-pair<int, int> solve()
-{
-<<<<<<< HEAD
-  scanf("%d %d %lld %lld %lld", &n, &t, &a0, &x, &y); now = a0;
-  for (int i = 0; i < t; i ++) scanf("%d", &tap[i]);
-
-=======
-  int app[(int) 1e6 + 1]; memset(app, -1, sizeof(app));
-  lli sum = 0; app[0] = 0;
-  for (int i = 0; i <= 2e6; i ++)
-  {
-    lli value = sequence[i % sequence.size()];
-    sum = (sum + value) % x;
-    if (app[sum] != -1)
-    {
-      if (i - app[sum] + (i == 0) >= y) return(make_pair(app[sum], i));
-    }
-    else
-      app[sum] = i + 1;
-  }
-  return(make_pair(-1, -1));
 }
 
 int main()
 {
-  scanf("%d %d %d %d %d", &n, &t, &a0, &x, &y);
+  scanf("%d %d %lld %lld %lld", &n, &t, &a0, &x, &y); now = a0;
   for (int i = 0; i < t; i ++) scanf("%d", &tap[i]);
 
-  sequence.push_back(a0);
-  generateSequence();
->>>>>>> 56eb7b497b6b3122088c1ce44e156402209ef6fa
   pair<int, int> ans = solve();
   printf("%d %d\n", ans.first, ans.second);
 
--- Maratona/2017/Regionals/judge.py
+++ Maratona/2017/Regionals/judge.py
@@ -17,16 +17,8 @@ compileCode(solver)
 print("Compiled Code", solver)
 inputPath, outputPath = testCasesPath + "/input/", testCasesPath + "/output/"
 testCases = os.listdir(inputPath)
-<<<<<<< HEAD
 for test in sorted(testCases, key=lambda x: int(x.split('_')[1])):
     startTime = time.time()
     os.system("./test < \"%s\" > auxOut" % (inputPath + test))
     executionTime = time.time() - startTime
-    print(test.split("_")[1], "- Verdict:", "Accpected" if cmp("auxOut", outputPath + test) and executionTime <= timeLimit else "Wrong Answer" if executionTime <= timeLimit else "Time Limit Exceeded")
-=======
-for test in testCases:
-    startTime = time.time()
-    os.system("./test < \"%s\" > auxOut" % (inputPath + test))
-    executionTime = time.time() - startTime
-    print("Verdict:", "Accpected" if cmp("auxOut", outputPath + test) and executionTime <= timeLimit else "Wrong Answer" if executionTime <= timeLimit else "Time Limit Exceeded")
->>>>>>> 56eb7b497b6b3122088c1ce44e156402209ef6fa
+    print(test.split("_")[1], "- Verdict:", "Accpected" if cmp("auxOut", outputPath + test) and executionTime <= timeLimit else "Wrong Answer" if executionTime <= timeLimit else "Time Limit Exceeded", "| time:", round(executionTime, 4))
