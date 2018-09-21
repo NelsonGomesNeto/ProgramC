7a1611820d1e665e552394aae1b82457b9968526
NelsonGomesNeto <ngn@ic.ufal.br>
2018-09-21 08:50:24 -0300

    It's not working .-., I wonder why...

--- Maratona/2009/Regionals/E (Dragster)/E.cpp
+++ Maratona/2009/Regionals/E (Dragster)/E.cpp
@@ -1,57 +1,120 @@
 #include <bits/stdc++.h>
+#define DEBUG if(0)
 using namespace std;
 double prob[301][301];
 int n, tree[602][2];
-vector<pair<int, double > > race[602];
+int nextRace[602];
+vector<pair<int, double> > race[602];
 
-void go(int u, vector<pair<int, double > >& r)
+void swap(int *a, int *b)
 {
-  if (tree[u][0] <= n && tree[u][1] <= 1)
+  int aux = *a;
+  *a = *b;
+  *b = *a;
+}
+
+void go(int u)
+{
+  if (u <= n) return;
+  if (tree[u][0] <= n && tree[u][1] <= n)
   {
-    r.push_back({tree[u][0], prob[tree[u][0]][tree[u][1]]});
-    r.push_back({tree[u][1], prob[tree[u][1]][tree[u][0]]});
+    race[u].push_back({tree[u][0], prob[tree[u][0]][tree[u][1]]});
+    race[u].push_back({tree[u][1], prob[tree[u][1]][tree[u][0]]});
     return;
   }
-  if (tree[u][0] > n)
-  go(tree[u][0], r);
-  go(tree[u][1], r);
+  go(tree[u][0]);
+  go(tree[u][1]);
+  if (tree[u][0] <= n)// && tree[u][1] > n)
+  {
+    double win = 0;
+    for (auto v: race[tree[u][1]])
+    {
+      win += prob[tree[u][0]][v.first] * v.second;
+      // printf("%d (%d) - %d %d\n", u, (int) race[u].size(), tree[u][0], v.first);
+      race[u].push_back({v.first, prob[v.first][tree[u][0]] * v.second});
+    }
+    race[u].push_back({tree[u][0], win});
+    return;
+  }
+  else
+  {
+    for (auto v: race[tree[u][0]])
+    {
+      double win = 0;
+      for (auto w: race[tree[u][1]])
+        win += prob[v.first][w.first] * w.second * v.second;
+      race[u].push_back({v.first, win});
+    }
+    for (auto w: race[tree[u][1]])
+    {
+      double win = 0;
+      for (auto v: race[tree[u][0]])
+        win += prob[w.first][v.first] * v.second * w.second;
+      race[u].push_back({w.first, win});
+    }
+    return;
+  }
 }
 
 int main()
 {
   while (scanf("%d", &n) != EOF && n)
   {
-    for (int i = 0; i < 602; i ++) race[i][0].clear(), race[i][1].clear();
     for (int i = 0; i < n; i ++)
       for (int j = 0; j < n; j ++)
         scanf("%lf", &prob[i + 1][j + 1]);
 
-    for (int i = 0; i < n - 1; i ++)
-      scanf("%d %d", &tree[n + i + 1][0], &tree[n + i + 1][1]);
-
-    for (int i = n + 1; i < 2 * n; i ++)
+    memset(nextRace, -1, sizeof(nextRace));
+    for (int i = n + 1; i < 2*n; i ++)
     {
-      go(i, race[i]);
+      scanf("%d %d", &tree[i][0], &tree[i][1]);
+      if (tree[i][1] <= n && tree[i][0] > n) swap(&tree[i][1], &tree[i][0]);
+      nextRace[tree[i][0]] = nextRace[tree[i][1]] = i;
+      race[i].clear();
     }
+
+    int root = 0, at = nextRace[1];
+    while (at != -1)
+    {
+      root = at;
+      at = nextRace[at];
+    }
+    go(root);
+    DEBUG for (int i = n + 1; i < 2*n; i ++)
+    {
+      printf("%d", i);
+      for (auto j: race[i])
+        printf(" (%d %.2lf)", j.first, j.second);
+      printf("\n");
+    }
+
+    int done = 0;
+    for (auto winner: race[root])
+      if (winner.first == 1)
+      {
+        printf("%.6lf\n", winner.second);
+        done = 1; break;
+      }
+    if (!done) printf("0.000000\n");
   }
   return(0);
 }
 /*
 
-  if (tree[i][0] > n) go(tree[i][0], race[i][0]);
-  else race[i][0].push_back({tree[i][0], 1});
-  if (tree[i][1] > n) go(tree[i][1], race[i][1]);
-  else race[i][1].push_back({tree[i][1], 1});
+  if (tree[i][0] > n) go(tree[i][0], nextRace[i][0]);
+  else nextRace[i][0].push_back({tree[i][0], 1});
+  if (tree[i][1] > n) go(tree[i][1], nextRace[i][1]);
+  else nextRace[i][1].push_back({tree[i][1], 1});
 }
 
 for (int i = n + 1; i < 2 * n; i ++)
 {
   printf("%d |", i);
-  for (int j = 0; j < race[i][0].size(); j ++)
-    printf(" %d", race[i][0][j]);
+  for (int j = 0; j < nextRace[i][0].size(); j ++)
+    printf(" %d", nextRace[i][0][j]);
   printf(" |");
-  for (int j = 0; j < race[i][1].size(); j ++)
-    printf(" %d", race[i][1][j]);
+  for (int j = 0; j < nextRace[i][1].size(); j ++)
+    printf(" %d", nextRace[i][1][j]);
   printf("\n");
 }
 printf("\n");
