1c8833a30806eea997577a01f35562e61c477b6d
NelsonGomesNeto <ngn@ic.ufal.br>
2018-09-27 21:58:51 -0300

    merge

--- Algorithms and Data Structures/Sparse Table/sparseTable.cpp
+++ Algorithms and Data Structures/Sparse Table/sparseTable.cpp
@@ -1,16 +1,4 @@
 #include <bits/stdc++.h>
-<<<<<<< HEAD
-using namespace std;
-const int maxLog = (int) floor(log2(1e6)) + 1;
-int sparseTable[(int) 1e6][maxLog];
-
-void printArray(int a[], int n)
-{
-  for (int i = 0; i < n; i ++) printf("%d%c", a[i], i < n - 1 ? ' ' : '\n');
-}
-
-int f(int a, int b)
-=======
 #define DEBUG if(0)
 #define lli long long int
 using namespace std;
@@ -24,34 +12,10 @@ void printArray(lli a[], int n)
 }
 
 lli f(lli a, lli b)
->>>>>>> 4dbf0ad5136bb20c3dbaf27a6ad88da70fa0cf8c
 {
   return(min(a, b));
 }
 
-<<<<<<< HEAD
-void build(int a[], int n, int logn)
-{
-  for (int i = 0; i < n; i ++) sparseTable[i][0] = a[i];
-
-  for (int j = 1; j <= logn; j ++)
-    for (int i = 0, prev = j - 1; i < n; i ++)
-    {
-      // printf("%d %d - (%d, %d) (%d, %d)\n", i, j, i, prev, min(n-1, i + (1 << j)), prev);
-      sparseTable[i][j] = f(sparseTable[i][prev], sparseTable[min(n-1, i + prev + 1)][prev]);
-    }
-}
-
-int query(int lo, int hi)
-{
-  int fim = 1 << 20;
-  for (int j = maxLog; j >= 0; j --)
-    if (lo + (1 << j) - 1 <= hi)
-    {
-      printf("%d %d\n", lo, j);
-      fim = f(fim, sparseTable[lo][j]);
-      lo += (1 << j) - 1;
-=======
 void build(lli array[], int n, int logn)
 {
   for (int i = 0; i < n; i ++) sparseTable[i][0] = f(array[i], array[i]);
@@ -68,7 +32,6 @@ lli query(int lo, int hi)
     {
       fim = f(fim, sparseTable[lo][j]);
       lo += (1 << j);
->>>>>>> 4dbf0ad5136bb20c3dbaf27a6ad88da70fa0cf8c
     }
   return(fim);
 }
@@ -76,21 +39,6 @@ lli query(int lo, int hi)
 int main()
 {
   int n; scanf("%d", &n);
-<<<<<<< HEAD
-  int array[n]; for (int i = 0; i < n; i ++) scanf("%d", &array[i]);
-  int logn = floor(log2(n));
-  printf("size: %d, logn: %d\n", n, logn);
-  printArray(array, n);
-
-  build(array, n, logn);
-  for (int i = 0; i < n; i ++) printArray(sparseTable[i], logn + 1);
-
-  int lo, hi;
-  while (scanf("%d %d", &lo, &hi) != EOF)
-  {
-    printf("[%d, %d] = %d\n", lo, hi, query(lo, hi));
-  }
-=======
   lli array[n]; for (int i = 0; i < n; i ++) scanf("%lld", &array[i]);
   int logn = floor(log2(n)) + 1;
   // printf("size: %d, logn: %d\n", n, logn);
@@ -101,7 +49,6 @@ int main()
   int lo, hi;
   while (scanf("%d %d", &lo, &hi) != EOF)
     printf("[%d, %d] = %lld\n", lo, hi, query(lo, hi));
->>>>>>> 4dbf0ad5136bb20c3dbaf27a6ad88da70fa0cf8c
 
   return(0);
 }
