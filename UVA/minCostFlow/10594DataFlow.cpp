3d7fccfb61ecba9ce8b79a173587986903ec6bc8
NelsonGomesNEto <ngn@ic.ufal.br>
2018-10-01 11:27:31 -0300

    FINALLY SOLVED DATA FLOW PROBLEM!!!

--- UVA/minCostFlow/10594DataFlow.cpp
+++ UVA/minCostFlow/10594DataFlow.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define lli long long int
 // source (0) -> nodes (1, n - 2) -> target (n - 1)
 const int maxVertices = 101;
-lli matrixGraph[maxVertices][maxVertices][2], inf = LLONG_MAX, cost[maxVertices];
+lli matrixGraph[maxVertices][maxVertices][2], inf = 1<<18, cost[maxVertices];
 int source = 0, target, vertices, previ[maxVertices];
 
 void addEdge(vector<int> graph[], int u, int v, lli f, lli c)
@@ -11,30 +11,26 @@ void addEdge(vector<int> graph[], int u, int v, lli f, lli c)
   graph[u].push_back(v);
   matrixGraph[u][v][0] = f, matrixGraph[u][v][1] = c;
   graph[v].push_back(u);
-  matrixGraph[v][u][0] = 0, matrixGraph[v][u][1] = -c;
+  matrixGraph[v][u][0] = f, matrixGraph[v][u][1] = c;
 }
 
 int bellmannFord(vector<int> graph[])
 {
-  for (int i = 0; i < vertices; i ++) cost[i] = LLONG_MAX;
+  for (int i = 0; i < vertices; i ++) cost[i] = inf;
   cost[source] = 0;
   for (int i = 0; i < vertices - 1; i ++)
   {
     int done = 0;
     for (int u = 0; u < vertices; u ++)
       for (auto v: graph[u])
-        if (matrixGraph[u][v][0] && cost[u] + matrixGraph[u][v][1] < cost[v])
+        if (matrixGraph[u][v][0] > 0 && cost[u] + matrixGraph[u][v][1] < cost[v])
         {
           cost[v] = cost[u] + matrixGraph[u][v][1];
           previ[v] = u, done = 1;
         }
     if (!done) break;
   }
-  for (int u = 0; u < vertices; u ++)
-    for (auto v: graph[u])
-      if (matrixGraph[u][v][0] && cost[u] + matrixGraph[u][v][1] < cost[v])
-        return(0);
-  return(cost[target] != LLONG_MAX);
+  return(cost[target] != inf);
 }
 
 pair<lli, lli> minCostFlow(vector<int> graph[])
@@ -42,25 +38,18 @@ pair<lli, lli> minCostFlow(vector<int> graph[])
   lli minCost = 0, maxFlow = 0;
   while (bellmannFord(graph))
   {
-    int v = target; lli flow = LLONG_MAX;
-    while (v != source)
-    {
-      printf("%d %d %lld %lld %lld\n", previ[v], v, flow, matrixGraph[previ[v]][v][0], matrixGraph[previ[v]][v][1]);
-      flow = min(flow, matrixGraph[previ[v]][v][0]);
-      v = previ[v];
-    }
-    if (!flow) break;
+    lli flow = LLONG_MAX;
+    for (int v = target; v != source; v = previ[v]) flow = min(flow, matrixGraph[previ[v]][v][0]);
     maxFlow += flow;
-    printf("Here %lld %lld %lld\n", maxFlow, minCost, flow);
+    // printf("Here %lld %lld %lld\n", maxFlow, minCost, flow);
 
-    v = target;
-    while (v != source)
+    for (int v = target; v != source; v = previ[v])
     {
       matrixGraph[previ[v]][v][0] -= flow;
       matrixGraph[v][previ[v]][0] += flow;
-      // printf("%d %d %lld\n", previ[v], v, matrixGraph[previ[v]][v][1]);
       minCost += matrixGraph[previ[v]][v][1] * flow;
-      v = previ[v];
+      matrixGraph[previ[v]][v][1] *= -1;
+      matrixGraph[v][previ[v]][1] *= -1;
     }
   }
   return(make_pair(minCost, maxFlow));
