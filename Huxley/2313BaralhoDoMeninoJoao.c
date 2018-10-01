8c005cbc8f01d8270e0f245086a4710e92d2ef12
NelsonGomesNeto <ngn@ic.ufal.br>
2018-10-01 18:29:16 -0300

    FINALLY, FINISHED ALL BACKTRACKING PROBLEMS FROM THEHUXLEY \O/

--- Huxley/2313BaralhoDoMeninoJoao.c
+++ Huxley/2313BaralhoDoMeninoJoao.c
@@ -16,27 +16,39 @@ void swap(card_t *a, card_t *b)
 
 void sort(card_t cards[], int n)
 {
-  for (int i = 0; i < n; i ++)
-    for (int j = i + 1; j < n; j ++)
+  int i, j;
+  for (i = 0; i < n; i ++)
+    for (j = i + 1; j < n; j ++)
       if (strcmp(cards[i].name, cards[j].name) > 0)
         swap(&cards[i], &cards[j]);
 }
 
+int getIndex(card_t cards[], char name[])
+{
+  int i = 0;
+  while (strcmp(cards[i].name, name)) i ++;
+  return(i);
+}
+
 int main()
 {
-  int n; scanf("%d", &n);
+  int n, i, j; scanf("%d", &n);
   card_t cards[n];
-  for (int i = 0; i < n; i ++)
+  for (i = 0; i < n; i ++)
   {
     scanf("\n%[^\n]", cards[i].name);
     scanf("%d", &cards[i].hSize);
-    for (int j = 0; j < cards[i].hSize; j ++)
+    for (j = 0; j < cards[i].hSize; j ++)
       scanf("%d", &cards[i].habilities[j]);
     scanf("\n%[^\n]", cards[i].nextCard);
   }
   sort(cards, n);
 
-  char name
+  char name[41]; scanf("\n%[^\n]", name);
+  int pos = getIndex(cards, name), at = 1;
+  printf("%d\n%d\n", pos, cards[pos].habilities[at - 1]);
+  for (i = getIndex(cards, cards[pos].nextCard); i != pos; i = getIndex(cards, cards[i].nextCard), at ++)
+    printf("%d\n%d\n", i, cards[i].habilities[at % cards[i].hSize]);
 
   return(0);
 }
