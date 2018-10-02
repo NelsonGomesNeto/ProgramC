3596c3698b622ff95be9fee845b6d8035ef6a76d
NelsonGomesNEto <ngn@ic.ufal.br>
2018-10-02 15:20:51 -0300

    In the end, it really was an accumulated sum problem hahaha

--- Maratona/Treino/Samara XI/H.cpp
+++ Maratona/Treino/Samara XI/H.cpp
@@ -1,18 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int si, sj, fi, fj;
-int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
-int n, m, d; vector<pair<int, int> > monsters;
+int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+int n, m, d;
 
-int invalid(int i, int j)
-{
-  for (int di = 0; di < monsters.size(); di ++)
-    if (abs(i - monsters[di].first) + abs(j - monsters[di].second) <= d)
-      return(1);
-  return(0);
-}
-
-int bfs(vector<char> maze[], vector<int> visited[])
+int bfs(vector<int> visited[], vector<int> accSum[])
 {
   deque<pair<pair<int, int>, int> > queue;
   queue.push_back({{si, sj}, 0});
@@ -21,8 +13,8 @@ int bfs(vector<char> maze[], vector<int> visited[])
     int i = queue.front().first.first, j = queue.front().first.second, steps = queue.front().second;
     if (i == fi && j == fj) return(steps);
     queue.pop_front();
-    if (i < 0 || j < 0 || i >= n || j >= m || visited[i][j] || invalid(i, j)) continue;
-    // visited[i][j] = 1;
+    if (i < 0 || j < 0 || i >= n || j >= m || visited[i][j] || accSum[i][j] != -1) continue;
+    visited[i][j] = 1;
     for (int k = 0; k < 4; k ++)
       queue.push_back({{i + dy[k], j + dx[k]}, steps + 1});
   }
@@ -32,22 +24,31 @@ int bfs(vector<char> maze[], vector<int> visited[])
 int main()
 {
   scanf("%d %d %d", &n, &m, &d);
-  vector<char> maze[n]; vector<int> visited[n]; char b;
+  vector<char> maze[n]; vector<int> visited[n], accSum[n]; char b;
   for (int i = 0; i < n; i ++)
   {
     getchar();
     for (int j = 0; j < m; j ++)
     {
       scanf("%c", &b);
-      maze[i].push_back(b);
-      if (maze[i][j] == 'S') si = i, sj = j;
-      if (maze[i][j] == 'F') fi = i, fj = j;
-      if (maze[i][j] == 'M') monsters.push_back({i, j});
-      visited[i].push_back(0);
+      maze[i].push_back(b); visited[i].push_back(0); accSum[i].push_back(-1);
+      if (b == 'S') si = i, sj = j;
+      if (b == 'F') fi = i, fj = j;
+      if (b == 'M') accSum[i][j] = d;
     }
   }
+  for (int i = 0; i < n; i ++)
+  {
+    for (int j = 1; j < m; j ++) accSum[i][j] = max(accSum[i][j - 1] - 1, accSum[i][j]);
+    for (int j = m - 2; j >= 0; j --) accSum[i][j] = max(accSum[i][j + 1] - 1, accSum[i][j]);
+  }
+  for (int j = 0; j < m; j ++)
+  {
+    for (int i = 1; i < n; i ++) accSum[i][j] = max(accSum[i - 1][j] - 1, accSum[i][j]);
+    for (int i = n - 2; i >= 0; i --) accSum[i][j] = max(accSum[i + 1][j] - 1, accSum[i][j]);
+  }
 
-  int ans = bfs(maze, visited);
+  int ans = accSum[fi][fj] == -1 ? bfs(visited, accSum) : -1;
   printf("%d\n", ans == -1 ? -1 : ans);
 
   return(0);
