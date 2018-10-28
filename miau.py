e8c5bee5fd6b31216b67f6a9c743bc41bf2c3d37
NelsonGomesNeto <ngn@ic.ufal.br>
2018-10-28 00:50:49 +0000

    DIJKSTRA IS A DP, OH MY, THAT WAS A GOOD PROBLEM. AND, PHYSICS PROBLEM THAT FELT GOOD, AWESOME

--- miau.py
+++ miau.py
@@ -1,36 +1,33 @@
-def countLost(blocks, visited):
-	now, F, R = 0, 0, 0
-	for b in blocks:
-		v = blocks[b]
-		if (not (v == 'F' or v == 'R') and b not in visited): now += 1
-		if (v == 'F'): F += 1
-		if (v == 'R'): R += 1
-	return(now, F, R)
+from turtle import *
+from math import acos
+import time
+pi = acos(-1)
+speed(0)
+delay(0)
+L = 7
 
-def go(blocks, now, path, visited):
-	path += [now]
-	if (now == 'L' or now == 'B' or now == 'F' or now == 'R'): return
-	visited.add(now)
-	go(blocks, blocks[now], path, visited)
+r = 21
 
-size = 80
-blocks = {}
-for i in range(80):
-	name = input()
-	next = input()
-	blocks[name] = next
-print(blocks, len(blocks))
+for r in range(14, 22):
+    o, i = r, r - L
+    so = 2 * pi * o
+    si = 2 * pi * i
+    speedo = 2 * pi * 3 * 3
+    speedi = 2 * pi * 3 * 2
+    print(so, o, si, i, so / speedo, si / speedi)
 
-size = 7
-files = {}
-for i in range(size):
-	name, start = input().split()
-	files[name] = start
+    for j in range(360):
+        forward(so / speedo)
+        right(1)
 
-visited = set()
-for f in files:
-	path = []
-	go(blocks, files[f], path, visited)
-	print("%s:" % f, ' -> '.join(path), "(íntegro)" if path[len(path) - 1] == 'L' else "(defeituoso)")
+    penup()
+    right(90)
+    forward(L)
+    left(90)
+    pendown()
 
-print("perdidos: %d (free: %d, reserved: %d, used: %d)" % (*countLost(blocks, visited), len(visited)))
+    for j in range(360):
+        forward(si / speedi)
+        right(1)
+
+    reset()
