4d130d049a4e50507bb76966eb0df0d10c4892eb
NelsonGomesNeto <ngn@ic.ufal.br>
2018-10-28 17:03:29 +0000

    LITE lazySegTree and Dubai simple RMQ done with Sparse Table

--- miau.py
+++ miau.py
@@ -1,33 +1,10 @@
-from turtle import *
-from math import acos
-import time
-pi = acos(-1)
-speed(0)
-delay(0)
-L = 7
+lines = []
+while (True):
+	try:
+		lines += [input()]
+	except:
+		break
+lines.sort()
+for line in lines:
+	print(line)
 
-r = 21
-
-for r in range(14, 22):
-    o, i = r, r - L
-    so = 2 * pi * o
-    si = 2 * pi * i
-    speedo = 2 * pi * 3 * 3
-    speedi = 2 * pi * 3 * 2
-    print(so, o, si, i, so / speedo, si / speedi)
-
-    for j in range(360):
-        forward(so / speedo)
-        right(1)
-
-    penup()
-    right(90)
-    forward(L)
-    left(90)
-    pendown()
-
-    for j in range(360):
-        forward(si / speedi)
-        right(1)
-
-    reset()
