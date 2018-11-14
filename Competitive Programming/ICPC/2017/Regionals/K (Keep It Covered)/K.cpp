1474b04debcbcb089872f328a14235c01c447ae6
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-11-14 11:05:11 -0300

    Now retrieval work, it's simpler and BEAUTIFUL

--- Competitive Programming/ICPC/2017/Regionals/K (Keep It Covered)/K.cpp
+++ Competitive Programming/ICPC/2017/Regionals/K (Keep It Covered)/K.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define RETRIEVE if(1)
+#define RETRIEVE if(0)
 using namespace std;
 // left, down, right, up
 int dy[4] = {0, 1, 0, -1}, dx[4] = {-1, 0, 1, 0};
@@ -13,24 +13,7 @@ int corner[4][2] = {{4, 5}, {6, 5}, {3, 6}, {3, 4}};
 int line[4] = {1, 2, 1, 2};
 int n, m;
 int puzz[20][20];
-
-int restriction(int i, int j, int k)
-{
-  if (!i && (k == 2 || k == 6 || k == 5 || k == 10 || k == 14)) return(1);
-  if (i == n - 1 && (k == 2 || k == 3 || k == 4 || k == 8 || k == 12)) return(1);
-  if (!j && (k == 1 || k == 3 || k == 6 || k == 7 || k == 11)) return(1);
-  if (j == m - 1 && (k == 1 || k == 4 || k == 5 || k == 9 || k == 13)) return(1);
-  return(0);
-}
-
-bool hasNegative()
-{
-  for (int i = 0; i < n; i ++)
-    for (int j = 0; j < m; j ++)
-      if (puzz[i][j] == -1)
-        return(true);
-  return(false);
-}
+map<pair<int, int>, int> mapDirToCorner = {{{0, 1}, 3}, {{0, 3}, 6}, {{1, 2}, 4}, {{2, 3}, 5}};
 
 const int maxVertices = 15*20*20 + 2, inf = 1<<20;
 // We're gonna map every single cell to every possible piece and configuration
@@ -102,91 +85,45 @@ int main()
   for (int i = 0; i < n; i ++)
     for (int j = 0; j < m; j ++)
     {
+      // 1 - horizontal, 2 - vertical
       if (puzzle[i][j] == 'o') dots ++;
-      if ((i + j) & 1) // odd piece
+      if ((i + j) & 1)
       {
-        addEdge(i*m + j, target, 1 + (puzzle[i][j] != 'o'));
-        if (puzzle[i][j] == 'o')
-        {
-          for (int k = 11; k <= 14; k ++)
-            if (!restriction(i, j, k))
-              addEdge(k*n*m + i*m + j, i*m + j, 1);
-        }
+        if (puzzle[i][j] == 'o') addEdge(i*m + j, target, 1);
         else
-          for (int k = 1; k < 11; k ++)
-            if (!restriction(i, j, k))
-              addEdge(k*n*m + i*m + j, i*m + j, 1 + (puzzle[i][j] != 'o'));
+        {
+          addEdge(1*n*m + i*m + j, 3*n*m + i*m + j, 2), addEdge(2*n*m + i*m + j, 3*n*m + i*m + j, 2);
+          addEdge(3*n*m + i*m + j, target, 2);
+        }
       }
       else
       {
-        addEdge(source, i*m + j, 1 + (puzzle[i][j] != 'o'));
-        if (puzzle[i][j] == 'o')
-        {
-          for (int k = 11; k <= 14; k ++)
-            if (!restriction(i, j, k))
-              addEdge(i*m + j, k*n*m + i*m + j, 1);
-        }
+        if (puzzle[i][j] == 'o') addEdge(source, i*m + j, 1);
         else
-          for (int k = 1; k < 11; k ++)
-            if (!restriction(i, j, k))
-              addEdge(i*m + j, k*n*m + i*m + j, 1 + (puzzle[i][j] != 'o'));
+        {
+          addEdge(source, 3*n*m + i*m + j, 2);
+          addEdge(3*n*m + i*m + j, 1*n*m + i*m + j, 2), addEdge(3*n*m + i*m + j, 2*n*m + i*m + j, 2);
+        }
 
         for (int k = 0; k < 4; k ++)
         {
           int ii = i + dy[k], jj = j + dx[k];
           if (ii < 0 || jj < 0 || ii >= n || jj >= m) continue;
-          // if (puzzle[i][j] != 'o' && puzzle[ii][jj] != 'o') // square - square
-          //   addEdge((7+k)*n*m + i*m + j, square[k]*n*m + ii*m + jj, 2);
-
-          if (puzzle[i][j] == 'o') // dot
+          if (puzzle[i][j] == 'o')
           {
-            if (puzzle[ii][jj] == 'o') // dot - dot
-              addEdge((11+k)*n*m + i*m + j, dot[k]*n*m + ii*m + jj, 1);
-            if (puzzle[ii][jj] != 'o')
-            {
-              // dot - corner
-              addEdge((11+k)*n*m + i*m + j, corner[k][0]*n*m + ii*m + jj, 1);
-              addEdge((11+k)*n*m + i*m + j, corner[k][1]*n*m + ii*m + jj, 1);
-              // dot - line
-              addEdge((11+k)*n*m + i*m + j, line[k]*n*m + ii*m + jj, 1);
-            }
+            if (puzzle[ii][jj] == 'o')
+              addEdge(i*m + j, ii*m + jj, 1);
+            else
+              addEdge(i*m + j, (1 + (k % 2))*n*m + ii*m + jj, 1);
           }
           else
           {
-            // line
-            if (puzzle[ii][jj] == 'o') // line - dot
-              addEdge(line[k]*n*m + i*m + j, dot[k]*n*m + ii*m + jj, 1);
+            if (puzzle[ii][jj] == 'o')
+              addEdge((1 + (k % 2))*n*m + i*m + j, ii*m + jj, 1);
             else
-            {
-              // line - line
-              addEdge(line[k]*n*m + i*m + j, line[k]*n*m + ii*m + jj, 1);
-              // line - corner
-              addEdge(line[k]*n*m + i*m + j, corner[k][0]*n*m + ii*m + jj, 1);
-              addEdge(line[k]*n*m + i*m + j, corner[k][1]*n*m + ii*m + jj, 1);
-            }
-            // corner
-            if (puzzle[ii][jj] == 'o') // corner - dot
-            {
-              addEdge(corner[(k + 2) % 4][0]*n*m + i*m + j, dot[k]*n*m + ii*m + jj, 1);
-              addEdge(corner[(k + 2) % 4][1]*n*m + i*m + j, dot[k]*n*m + ii*m + jj, 1);
-            }
-            else
-            {
-              // corner - line
-              addEdge(corner[(k + 2) % 4][0]*n*m + i*m + j, line[k]*n*m + ii*m + jj, 1);
-              addEdge(corner[(k + 2) % 4][1]*n*m + i*m + j, line[k]*n*m + ii*m + jj, 1);
-              // corner - corner
-              addEdge(corner[(k + 2) % 4][0]*n*m + i*m + j, corner[k][0]*n*m + ii*m + jj, 1);
-              addEdge(corner[(k + 2) % 4][0]*n*m + i*m + j, corner[k][1]*n*m + ii*m + jj, 1);
-              addEdge(corner[(k + 2) % 4][1]*n*m + i*m + j, corner[k][0]*n*m + ii*m + jj, 1);
-              addEdge(corner[(k + 2) % 4][1]*n*m + i*m + j, corner[k][1]*n*m + ii*m + jj, 1);
-            }
+              addEdge((1 + (k % 2))*n*m + i*m + j, (1 + (k % 2))*n*m + ii*m + jj, 2);
           }
         }
-        // line (1 (horizontal) : 2 (vertical)) flow 2
-        // corner (3 (left, down), 4 (right, down), 5 (right, up), 6 (left, up)) flow 2
-        // square (7 (left), 8 (down), 9 (right), 10 (up)) flow 2
-        // dot (11 (left), 12 (down), 13 (right), 14 (up)) flow 1
       }
     }
 
@@ -195,74 +132,95 @@ int main()
   printf("%s\n", ans == (n*m - (dots / 2)) ? "Y" : "N");
   RETRIEVE
   {
-    for (Edge &cell: graph[source])
-      if (!cell.flow)
+    memset(puzz, -1, sizeof(puzz));
+    for (int i = 0; i < n; i ++)
+      for (int j = 0; j < m; j ++)
       {
-        int chosen = -1, i = cell.to / m, j = cell.to % m;
-        for (Edge &config: graph[cell.to])
-          if (!config.flow)
-            chosen = (config.to - cell.to) / (n*m);
-        puzz[i][j] = chosen;
-      }
-    for (Edge &cell: graph[target])
-      if (!graph[cell.to][cell.back].flow)
-      {
-        int chosen = -1, i = cell.to / m, j = cell.to % m;
-        for (Edge &config: graph[cell.to])
-          if (!graph[config.to][config.back].flow)
-            chosen = (config.to - cell.to) / (n*m);
-        puzz[i][j] = chosen;
-      }
-    while (hasNegative() && 0)
-      for (int i = 0; i < n; i ++)
-        for (int j = 0; j < m; j ++)
-          if (puzz[i][j] == -1)
-          {
-            if (i > 0 && (puzz[i - 1][j] == 2 || puzz[i - 1][j] == 3 || puzz[i - 1][j] == 4 || puzz[i - 1][j] == 12))
+        if ((i + j) & 1)
+        {
+          if (puzzle[i][j] == 'o')
+            for (Edge &e: graph[i*m + j])
             {
-              if (j > 0 && (puzz[i][j - 1] == 1 || puzz[i][j - 1] == 4 || puzz[i][j - 1] == 5 || puzz[i][j - 1] == 13))
-                puzz[i][j] = 6;
-              else
-                puzz[i][j] = 5;
+              if (e.flow)
+              {
+                int ii = (e.to % (n*m)) / m, jj = (e.to % (n*m)) % m;
+                for (int k = 0; k < 4; k ++)
+                  if (ii == i + dy[k] && jj == j + dx[k])
+                    puzz[i][j] = 11+k;
+              }
             }
-            else if (i > 0 && j < m - 1 && (puzz[i - 1][j] == 1 || puzz[i - 1][j] == 5 || puzz[i - 1][j] == 6 || (puzz[i - 1][j] >= 11 && puzz[i - 1][j] != 12)))
-              puzz[i][j] = 1;
+          else
+          {
+            vector<int> dir;
+            for (Edge &e: graph[3*n*m + i*m + j])
+              if (e.flow != e.capacity)
+              {
+                for (Edge &ee: graph[e.to])
+                  if (ee.flow != ee.capacity)
+                  {
+                    int ii = (ee.to % (n*m)) / m, jj = (ee.to % (n*m)) % m;
+                    for (int k = 0; k < 4; k ++)
+                    if (ii == i + dy[k] && jj == j + dx[k])
+                    dir.push_back(k);
+                  }
+              }
+            printf("[%d, %d] %c == %d\n", i, j, puzzle[i][j], (int) dir.size());
+            printf("\t");
+            for (int k = 0; k < dir.size(); k ++) printf("%d%c", dir[k], k < dir.size() - 1 ? ' ' : '\n');
+            if (dir.size() == 1) puzz[i][j] = 7+dir[0];
+            else if (dir[0] % 2 == dir[1] % 2) puzz[i][j] = 1 + (dir[0] % 2);
             else
             {
-              if (j > 0 && (puzz[i][j - 1] == 1 || puzz[i][j - 1] == 4 || puzz[i][j - 1] == 5 || puzz[i][j - 1] == 13))
-                puzz[i][j] = 3;
-              else
-                puzz[i][j] = 4;
+              sort(dir.begin(), dir.end());
+              puzz[i][j] = mapDirToCorner[{dir[0], dir[1]}];
             }
           }
+        }
+        else
+        {
+          if (puzzle[i][j] == 'o')
+            for (Edge &e: graph[i*m + j])
+            {
+              if (!e.flow)
+              {
+                int ii = (e.to % (n*m)) / m, jj = (e.to % (n*m)) % m;
+                for (int k = 0; k < 4; k ++)
+                  if (ii == i + dy[k] && jj == j + dx[k])
+                    puzz[i][j] = 11+k;
+              }
+            }
+          else
+          {
+            vector<int> dir;
+            for (Edge &e: graph[3*n*m + i*m + j])
+              if (e.flow != e.capacity)
+              {
+                for (Edge &ee: graph[e.to])
+                  if (ee.flow != ee.capacity)
+                  {
+                    int ii = (ee.to % (n*m)) / m, jj = (ee.to % (n*m)) % m;
+                    for (int k = 0; k < 4; k ++)
+                    if (ii == i + dy[k] && jj == j + dx[k])
+                    dir.push_back(k);
+                  }
+              }
+            printf("[%d, %d] %c == %d\n", i, j, puzzle[i][j], (int) dir.size());
+            printf("\t");
+            for (int k = 0; k < dir.size(); k ++) printf("%d%c", dir[k], k < dir.size() - 1 ? ' ' : '\n');
+            if (dir.size() == 1) puzz[i][j] = 7+dir[0];
+            else if (dir[0] % 2 == dir[1] % 2) puzz[i][j] = 1 + (dir[0] % 2);
+            else
+            {
+              sort(dir.begin(), dir.end());
+              puzz[i][j] = mapDirToCorner[{dir[0], dir[1]}];
+            }
+          }
+        }
+      }
     printf("%d %d\n", n, m);
     for (int i = 0; i < n; i ++)
       for (int j = 0; j < m; j ++)
         printf("%d%c", puzz[i][j], j < m - 1 ? ' ' : '\n');
-
-    for (int i = 0; i < n; i ++)
-      for (int j = 0; j < m; j ++)
-        if (puzzle[i][j] == 'o')
-        {
-          printf("[%d, %d] %d\n", i, j, (i + j) & 1);
-          for (Edge &e: graph[i*m + j])
-          {
-            if (e.to != source && e.to != target) printf("\t%d %d %d\n", e.to / (n*m), e.to, e.flow);
-            if ((((i + j) & 1) && !graph[e.to][e.back].flow) || (!((i + j) & 1) && !e.flow))
-              for (Edge &ee: graph[e.to])
-              {
-                printf("\t\t%d %d\n", ee.to, ee.flow);
-                if ((((i + j) & 1) && !graph[ee.to][ee.back].flow) || (!((i + j) & 1) && !ee.flow))
-                  for (Edge &eee: graph[ee.to])
-                  {
-                    if ((i + j) & 1 && eee.capacity)
-                      printf("\t\t\t%d [%d, %d] %d\n", (ee.to - eee.to) / (n*m), ee.to / m, ee.to % m, ee.to, eee.flow);
-                    else if (eee.capacity)
-                      printf("\t\t\t%d [%d, %d] %d\n", (ee.to - eee.to) / (n*m), eee.to / m, eee.to % m, eee.to, eee.flow);
-                  }
-              }
-          }
-        }
   }
   return(0);
 }
