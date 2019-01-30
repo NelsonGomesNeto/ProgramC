ee7bcbc442303072cb2e81941bae065b48b66e51
NelsonGomesNeto <ngn@ic.ufal.br>
2019-01-30 00:00:04 -0300

    Adds 148AInsomniaCure (CodeForces), very easy but funny problem hahaha

--- Scratch/test.c
+++ Scratch/test.c
@@ -1,17 +1,201 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-char* func()
+typedef struct n
 {
-  char *string = (char*) malloc(10 * sizeof(char));
-  strcat(string, "miau\0");
-  return(string);
+    void *item;
+    struct n *next;
+} node;
+
+typedef struct
+{
+    int num;
+} data;
+
+node *add_node(node *head, void *item)
+{
+    node *new_node = (node *)(malloc(sizeof(node)));
+    new_node->item = item;
+    new_node->next = head;
+
+    return new_node;
+}
+
+node *add_node_incre(node *head, void *item, int (*f_compair)(void *item_1, void *item_2))
+{
+    node *current = head, *previous = NULL;
+    node *new_node = (node *)(malloc(sizeof(node)));
+    new_node->item = item;
+
+    if (current == NULL)
+    {
+        new_node->next = NULL;
+        head = new_node;
+    }
+    else
+    {
+        while (current != NULL && f_compair(item, current->item) == 1)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        new_node->next = current;
+
+        if (previous == NULL)
+        {
+            head = new_node;
+        }
+        else
+        {
+            previous->next = new_node;
+        }
+    }
+
+    return head;
+}
+
+void clear_list(node *head)
+{
+    node *node_aux;
+
+    while (head != NULL)
+    {
+        node_aux = head->next;
+        free(head);
+        head = node_aux;
+    }
+}
+
+node *search_list(node *head, void *item, int (*compair)(void *item1, void *item2))
+{
+    while ((head != NULL) && ((*compair)(head->item, item) != 1))
+    {
+        head = head->next;
+    }
+
+    return head;
+}
+
+node *check_intersection_list(node *head_1, node *head_2, int (*f_equal)(void *item_1, void *item_2), void *(*f_copy)(void *item), int (*f_hightest)(void *item_1, void *item_2))
+{
+    node *head_inters = NULL;
+    node *aux_head_2;
+
+    while (head_1 != NULL)
+    {
+        aux_head_2 = head_2;
+
+        while (aux_head_2 != NULL)
+        {
+            if ((f_equal(head_1->item, aux_head_2->item) == 1))// && (search_list(head_inters, head_1->item, f_equal) == NULL))
+            {
+                head_inters = add_node(head_inters, f_copy(head_1->item));
+                // head_inters = add_node_incre(head_inters, f_copy(head_1->item), f_hightest);
+            }
+
+            aux_head_2 = aux_head_2->next;
+        }
+
+        head_1 = head_1->next;
+    }
+
+    return head_inters;
+}
+
+void apply_all_list(node *head, void (*function)(void *item))
+{
+    while (head != NULL)
+    {
+        function(head->item);
+
+        head = head->next;
+    }
+}
+
+int compair_equal_data(void *_item_1, void *_item_2)
+{
+    data *item_1 = (data *)_item_1;
+    data *item_2 = (data *)_item_2;
+
+    if (item_1->num == item_2->num)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int compair_hightest_data(void *_node_1, void *_node_2)
+{
+    data *node_1 = (data *)_node_1;
+    data *node_2 = (data *)_node_2;
+
+    if (node_1->num > node_2->num)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+data *generate_data(int num)
+{
+    data *new_data = (data *)(malloc(sizeof(data)));
+    new_data->num = num;
+
+    return new_data;
+}
+
+void *copy_data(void *_old_data)
+{
+    data *old_data = (data *)_old_data;
+    data *new_data = (data *)(malloc(sizeof(data)));
+
+    new_data->num = old_data->num;
+
+    return new_data;
+}
+
+void print_all_data(void *_item)
+{
+    data *item = (data *)_item;
+
+    printf("%d\n", item->num);
 }
 
 int main()
 {
-  char *string = func();
-  printf("%s\n", string);
-  return(0);
+    int i, aux;
+    node *head_1 = NULL;
+    node *head_2 = NULL;
+
+    for (i = 0; i < 20; i++)
+    {
+        scanf("%d", &aux);
+        head_1 = add_node(head_1, generate_data(aux));
+    }
+
+    for (i = 0; i < 20; i++)
+    {
+        scanf("%d", &aux);
+        head_2 = add_node(head_2, generate_data(aux));
+    }
+
+    node *head_inters = check_intersection_list(head_1, head_2, compair_equal_data, copy_data, compair_hightest_data);
+
+    if (head_inters != NULL)
+    {
+        apply_all_list(head_inters, print_all_data);
+    }
+    else
+    {
+        printf("VAZIO\n");
+    }
+
+    return 0;
 }
--- Scratch/test.cpp
+++ Scratch/test.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Num
+{
+  int num;
+  bool operator<(const Num &a) const { return(num < a.num); }
+};
+
 int main()
 {
-  vector<int> a;
-  a.push_back(1);
-  int pos = a.end() - a.begin();
-  printf("%d\n", pos);
+  Num one = {1}, two = {2};
+  vector<Num> lol = {{1}, {2}};
+  sort(lol.begin(), lol.end());
+  printf("%d\n", (int) (one < two));
+  printf("%d %d\n", lol[0].num, lol[1].num);
   return(0);
 }
