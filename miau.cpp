03b983f78a2aa2866fb9865c8ee493b749f8e054
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-02-15 15:16:42 -0300

    Interesting ad hoc problem ^~^

--- miau.cpp
+++ miau.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 
+int n, m;
+
+void func(int mat[][])
+{
+  for (int i = 0; i < n; i ++)
+    for (int j = 0; j < m; j ++)
+      printf("%d%c", mat[i][j], j < 10 - 1 ? ' ' : '\n');
+}
+
 int main()
 {
-  bool a[10]; memset(a, false, sizeof(a)); memset(a, true, sizeof(a));
-  for (int i = 0; i < 10; i ++) printf("%d\n", a[i]);
+  scanf("%d %d", &n, &m);
+  int mat[n][m];
+  for (int i = 0, s = 0; i < n; i ++)
+    for (int j = 0; j < m; j ++)
+      mat[i][j] = s ++;
+  func(&mat[0][0]);
   return(0);
 }
