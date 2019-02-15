5654d3125eff98ed78e261bc9aace56bde5ae7b5
NelsonGomesNeto <ngn@ic.ufal.br>
2019-02-15 09:39:34 -0300

    git add .

--- Online Judges/Huxley/triangulodepascal.c
+++ Online Judges/Huxley/triangulodepascal.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  int tam, num = 1, run, tri; scanf("%d", &tam);
-  for (run = 0; run <= tam; run ++)
+  int n; scanf("%d", &n);
+  int prv[n + 1], nxt[n + 1], i, j; prv[0] = prv[1] = 1;
+  if (n) printf("1\n1 1\n"); else printf("1\n");
+  for (i = 1; i < n; i ++)
   {
-    int soma[run];
-    for (tri = 0; tri <= run; tri ++)
-    {
-      printf("%d\n", num);
-      soma[tri] =
-    }
+    nxt[0] = nxt[i + 1] = 1;
+    for (j = 0; j < i; j ++)
+      nxt[j + 1] = prv[j] + prv[j + 1];
+    for (j = 0; j <= i + 1; j ++)
+      printf("%d%c", nxt[j], j < i + 1 ? ' ' : '\n');
+    memcpy(prv, nxt, sizeof(prv));
   }
   return(0);
 }
