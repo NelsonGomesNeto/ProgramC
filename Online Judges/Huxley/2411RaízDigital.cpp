745df38e8f83ac7bd334cb30f29266be77488dff
NelsonGomesNeto <ngn@ic.ufal.br>
2019-02-16 00:33:34 -0300

    P1 2018.2 3rd test

---
+++ Online Judges/Huxley/2411RaízDigital.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+int sumDigits(int num)
+{
+  int sum = 0;
+  while (num > 0)
+  {
+    sum += num % 10;
+    num /= 10;
+  }
+  return(sum);
+}
+
+int digitalRoot(int num)
+{
+  while (num > 9)
+  {
+    num = sumDigits(num);
+  }
+  return(num);
+}
+
+int main()
+{
+  int n; scanf("%d", &n);
+  while (n --)
+  {
+    int k, x; scanf("%d %d", &k, &x);
+    int i = 0, found = 0;
+    while (found < k)
+    {
+      i = i + 1;
+      if (digitalRoot(i) == x) found ++;
+    }
+      
+    printf("%d\n", i);
+  }
+  return(0);
+}
