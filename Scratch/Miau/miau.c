ff927e94dcf38e8efaf0e0d44d9e827e1db39735
NelsonGomesNeto <ngn@ic.ufal.br>
2019-04-27 17:29:57 -0300

    git add .

--- Scratch/Miau/miau.c
+++ Scratch/Miau/miau.c
@@ -1,22 +1,59 @@
 #include <stdio.h>
+typedef struct Person person_t;
 
-void swap(int *a, int *b)
+struct Person
 {
-  int aux = *a;
+  int id;
+  char name[100];
+  double grade;
+};
+
+void printPeople(int n, person_t people[], double mean)
+{
+  printf("Alunos abaixo da media:\n");
+  int i;
+  for (i = 0; i < n; i ++)
+    if (people[i].grade < 7)
+      printf("Matricula: %d Nome: %s Nota: %.1lf\n", people[i].id, people[i].name, people[i].grade);
+  printf("Alunos iguais ou acima da media:\n");
+  for (i = 0; i < n; i ++)
+    if (people[i].grade >= 7)
+      printf("Matricula: %d Nome: %s Nota: %.1lf\n", people[i].id, people[i].name, people[i].grade);
+  printf("Media = %.2lf\n", mean);
+}
+
+void swap(person_t *a, person_t *b)
+{
+  person_t aux = *a;
   *a = *b;
   *b = aux;
 }
 
+void sort(int n, person_t people[])
+{
+  int i, j;
+  for (i = 0; i < n; i ++)
+    for (j = i + 1; j < n; j ++)
+      if (people[i].grade > people[j].grade || (people[i].grade == people[j].grade && people[i].id > people[j].id))
+        swap(&people[i], &people[j]);
+}
+
 int main()
 {
-  int a, b, c; scanf("%d %d %d", &a, &b, &c);
-  if (a > b) swap(&a, &b);
-  if (a > c) swap(&a, &c);
-  if (b > c) swap(&b, &c);
-  int h, l; scanf("%d %d", &h, &l);
-  if (h > l) swap(&h, &l);
+  person_t p;
+  int n, i;
+  double mean = 0;
+  scanf("%d", &n);
+  person_t people[n];
+  for (i = 0; i < n; i ++)
+  {
+    scanf("%d-%[^-]-%lf", &people[i].id, people[i].name, &people[i].grade);
+    mean += people[i].grade;
+  }
+  mean /= n;
 
-  printf("%c\n", (a <= h && b <= l) ? 'S' : 'N');
+  sort(n, people);
+  printPeople(n, people, mean);
 
   return(0);
 }
--- Scratch/test.cpp
+++ Scratch/test.cpp
@@ -1,41 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-const double pi = acos(-1);
-double degToRad(double angle) { return(angle * pi / 180.0); }
-
-struct X { double d, theta, wetted; };
-
-double wettedPerimeter(double d, double theta)
-{
-  // sin = CO / H
-  // H = sin * CO
-  // tan = CO / CA
-  // CA = CO / tan
-  double h = d / sin(theta), ca = d / tan(theta);
-  double wetted = 2 * h;
-  double smallSide = 50.0 / d - ca / 2.0;
-  // printf("%lf %lf %lf %lf %lf\n", d, theta, h, wetted, smallSide);
-  if (smallSide < 0) return(1e9);
-  return(wetted + smallSide);
-  // area = (2*b + d / tan(theta)) * d / 2
-  // 2 * area / d = 2*b + d / tan(theta)
-  // 2* area / d - d / tan(theta) = 2 * b
-  // area / d - d / (2*tan(theta)) =  b
-}
 
 int main()
 {
-  X best; best.wetted = 1e9;
-  // wettedPerimeter(4, degToRad(10));
-  for (double d = 1; d < 50; d ++)
-    for (double theta = 1; theta <= 90; theta ++)
-    {
-      double ans = wettedPerimeter(d, degToRad(theta));
-      if (ans < best.wetted && ans > 0)
-      {
-        printf("(%3.3lf, %3.3lf) = %lf\n", d, theta, wettedPerimeter(d, degToRad(theta)));
-        best = {d, theta, ans};
-      }
-    }
   return(0);
 }
