fd2cc5d05360c0a4512d4952b9b54a19c0865a5e
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-06-14 10:43:12 -0300

    Adds QuadTree implementation to Geometry

--- Algorithms and Data Structures/Geometry/geometry.cpp
+++ Algorithms and Data Structures/Geometry/geometry.cpp
@@ -33,3 +33,44 @@ struct Point
   bool collinear(const Point &a, const Point &b) { return((x - a.x)*(a.y - b.y) - (a.x - b.x)*(y - a.y) == 0); }
 };
 */
+/*
+struct QuadTree
+{
+  int inside;
+  QuadTree *quadrants[4];
+};
+QuadTree *root;
+QuadTree* newQuadTree(int lox = minX, int loy = minY, int hix = maxX, int hiy = maxY)
+{
+  if (lox > hix || loy > hiy) return(NULL);
+  QuadTree *temp = (QuadTree*) malloc(sizeof(QuadTree));
+  temp->inside = 0;
+  for (int i = lowerBound(lox); i < n && points[i].x <= hix; i ++)
+    temp->inside += points[i].inside(lox, loy, hix, hiy);
+  if ((lox == hix && loy == hiy) || temp->inside == 0)
+  {
+    for (int i = 0; i < 4; i ++) temp->quadrants[i] = NULL;
+    return(temp);
+  }
+  int midx = (lox + hix) >> 1, midy = (loy + hiy) >> 1;
+  temp->quadrants[0] = newQuadTree(lox, loy, midx, midy);
+  temp->quadrants[1] = newQuadTree(lox, midy + 1, midx, hiy);
+  temp->quadrants[2] = newQuadTree(midx + 1, midy + 1, hix, hiy);
+  temp->quadrants[3] = newQuadTree(midx + 1, loy, hix, midy);
+  return(temp);
+}
+
+int query(int qlox, int qloy, int qhix, int qhiy, int lox = minX, int loy = minY, int hix = maxX, int hiy = maxY, QuadTree *curr = root)
+{
+  if (lox > hix || loy > hiy || curr == NULL) return(0);
+  if (qlox > hix || qhix < lox || qloy > hiy || qhiy < loy) return(0);
+  if (lox >= qlox && loy >= qloy && hix <= qhix && hiy <= qhiy) return(curr->inside);
+  int ans = 0;
+  int midx = (lox + hix) >> 1, midy = (loy + hiy) >> 1;
+  ans += query(qlox, qloy, qhix, qhiy, lox, loy, midx, midy, curr->quadrants[0]);
+  ans += query(qlox, qloy, qhix, qhiy, lox, midy + 1, midx, hiy, curr->quadrants[1]);
+  ans += query(qlox, qloy, qhix, qhiy, midx + 1, midy + 1, hix, hiy, curr->quadrants[2]);
+  ans += query(qlox, qloy, qhix, qhiy, midx + 1, loy, hix, midy, curr->quadrants[3]);
+  return(ans);
+}
+*/
